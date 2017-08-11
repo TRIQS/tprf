58b3f69b67fbc29dbc26e36c6b6120abd82eef77
Hugo U. R. Strand <hugo.strand@gmail.com>
2017-08-11 16:54:55 +0200

    [test] linalg in all channels

--- test/c++/linalg_ops.cpp
+++ test/c++/linalg_ops.cpp
@@ -32,80 +32,104 @@ using namespace triqs::gfs;
 using namespace triqs::arrays;
 using namespace tprf;
 
-// Fill two-particle Green's function with random complex numbers Re/Im in [-1, 1]
-void random_fill(g2_iw_t& G, int rng_seed = 23432) {
- typedef size_t st;
- triqs::mc_tools::random_generator RNG("mt19937", rng_seed);
- assign_foreach(G.data(),
-                [&RNG](st n1, st n2, st n3, st i, st j, st k, st l) { return std::complex<double>(RNG(2.) - 1., RNG(2.) - 1.); });
+// ----------------------------------------------------
+// Helper functions
+
+// Fill two-particle Green's function with random complex numbers Re/Im in [-1,
+// 1]
+void random_fill(g2_iw_t &G, int rng_seed = 23432) {
+  typedef size_t st;
+  triqs::mc_tools::random_generator RNG("mt19937", rng_seed);
+  assign_foreach(G.data(), [&RNG](st n1, st n2, st n3, st i, st j, st k, st l) {
+    return std::complex<double>(RNG(2.) - 1., RNG(2.) - 1.);
+  });
 }
 
-void print(g2_iw_t const& G) {
- typedef size_t st;
- auto data = G.data();
- foreach (data, [&data](st n1, st n2, st n3, st i, st j, st k, st l) {
-  std::cout << "data(" << n1 << ", " << n2 << ", " << n3 << "; " << i << ", " << j << ", " << k << ", " << l
-            << ") = " << data(n1, n2, n3, i, j, k, l) << "\n";
- })
-  ;
+void print(g2_iw_t const &G) {
+  typedef size_t st;
+  auto data = G.data();
+  foreach (data, [&data](st n1, st n2, st n3, st i, st j, st k, st l) {
+    std::cout << "data(" << n1 << ", " << n2 << ", " << n3 << "; " << i << ", "
+              << j << ", " << k << ", " << l
+              << ") = " << data(n1, n2, n3, i, j, k, l) << "\n";
+  })
+    ;
 }
 
-TEST(CtHyb, ph_identity_product) {
+// ----------------------------------------------------
 
- double beta = 1.2345;
- int nwb = 4, nwf = 6, norb = 3;
+// TESTS
 
- gf_mesh<imfreq> fmesh{beta, Boson, nwb};
- gf_mesh<imfreq> bmesh{beta, Fermion, nwf};
- g2_iw_t A{{bmesh, fmesh, fmesh}, {norb, norb, norb, norb}};
+// ----------------------------------------------------
+// Test product with identity operator I = I * A = A * I
 
- random_fill(A);
- // print(A);
+template <Channel_t CH> void identity_product_impl() {
+  double beta = 1.2345;
+  int nwb = 4, nwf = 6, norb = 3;
 
- constexpr Channel_t CH = Channel_t::PH;
+  gf_mesh<imfreq> fmesh{beta, Boson, nwb};
+  gf_mesh<imfreq> bmesh{beta, Fermion, nwf};
+  g2_iw_t A{{bmesh, fmesh, fmesh}, {norb, norb, norb, norb}};
 
- auto I = identity<CH>(A);
+  random_fill(A);
+  // print(A);
 
- // Test identity and product I * A = A
- {
-  auto I_A = product<CH>(I, A);
-  EXPECT_GF_NEAR(I_A, A);
- }
+  auto I = identity<CH>(A);
 
- // Test identity and product A * I = A
- {
-  auto A_I = product<CH>(A, I);
-  EXPECT_GF_NEAR(A_I, A);
- }
+  // Test identity and product I * A = A
+  {
+    auto I_A = product<CH>(I, A);
+    EXPECT_GF_NEAR(I_A, A);
+  }
+
+  // Test identity and product A * I = A
+  {
+    auto A_I = product<CH>(A, I);
+    EXPECT_GF_NEAR(A_I, A);
+  }
 }
 
-TEST(CtHyb, ph_inverse_product) {
-
- double beta = 1.2345;
- int nwb = 4, nwf = 6, norb = 3;
-
- gf_mesh<imfreq> fmesh{beta, Boson, nwb};
- gf_mesh<imfreq> bmesh{beta, Fermion, nwf};
- g2_iw_t A{{bmesh, fmesh, fmesh}, {norb, norb, norb, norb}};
-
- random_fill(A);
-
- constexpr Channel_t CH = Channel_t::PH;
-
- auto A_inv = inverse<CH>(A);
- auto I = identity<CH>(A);
-
- // Test A * A_inv = I
- {
-  auto A_A_inv = product<CH>(A, A_inv);
-  EXPECT_GF_NEAR(A_A_inv, I);
- }
-
- // Test identity and product A_inv * A = I
- {
-  auto A_inv_A = product<CH>(A_inv, A);
-  EXPECT_GF_NEAR(A_inv_A, I);
- }
+TEST(CtHyb, ph_identity_product) { identity_product_impl<Channel_t::PH>(); }
+TEST(CtHyb, pp_identity_product) { identity_product_impl<Channel_t::PP>(); }
+TEST(CtHyb, ph_bar_identity_product) {
+  identity_product_impl<Channel_t::PH_bar>();
 }
 
+// ----------------------------------------------------
+// Test product with inverse operator I = A^{-1} * A = A * A^{-1}
+
+template <Channel_t CH> void inverse_product_impl() {
+
+  double beta = 1.2345;
+  int nwb = 4, nwf = 6, norb = 3;
+
+  gf_mesh<imfreq> fmesh{beta, Boson, nwb};
+  gf_mesh<imfreq> bmesh{beta, Fermion, nwf};
+  g2_iw_t A{{bmesh, fmesh, fmesh}, {norb, norb, norb, norb}};
+
+  random_fill(A);
+
+  auto A_inv = inverse<CH>(A);
+  auto I = identity<CH>(A);
+
+  // Test A * A_inv = I
+  {
+    auto A_A_inv = product<CH>(A, A_inv);
+    EXPECT_GF_NEAR(A_A_inv, I);
+  }
+
+  // Test identity and product A_inv * A = I
+  {
+    auto A_inv_A = product<CH>(A_inv, A);
+    EXPECT_GF_NEAR(A_inv_A, I);
+  }
+}
+
+TEST(CtHyb, ph_inverse_product) { inverse_product_impl<Channel_t::PH>(); }
+TEST(CtHyb, pp_inverse_product) { inverse_product_impl<Channel_t::PP>(); }
+TEST(CtHyb, ph_bar_inverse_product) {
+  inverse_product_impl<Channel_t::PH_bar>();
+}
+
+// ----------------------------------------------------
 MAKE_MAIN;
