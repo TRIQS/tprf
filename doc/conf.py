3e96757126ada9af69e931b1cac3b2fe50da41a5
Hugo U. R. Strand <hugo.strand@gmail.com>
2017-12-12 15:16:56 +0100

    [doc] readthedocs conf.py

---
+++ doc/conf.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from recommonmark.parser import CommonMarkParser
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    ]
+
+templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']		
+source_parsers = {		
+            '.md': CommonMarkParser,		
+        }
+master_doc = 'index'
+project = u'tprf'
+copyright = u'2016'
+version = 'latest'
+release = 'latest'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'tprf'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'tprf.tex', u'tprf Documentation',
+   u'', 'manual'),
+]
