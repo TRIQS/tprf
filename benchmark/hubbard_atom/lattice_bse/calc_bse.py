66a530c635e150c89c186b5cc9e5b2565a4dde66
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-06-13 14:48:26 +0200

    [bench] hub atom bse test update

--- benchmark/hubbard_atom/lattice_bse/calc_bse.py
+++ benchmark/hubbard_atom/lattice_bse/calc_bse.py
@@ -1,35 +1,25 @@
 # ----------------------------------------------------------------------
 
-import glob
 import numpy as np
 
 # ----------------------------------------------------------------------
 
-from pytriqs.gf import *
-from pytriqs.plot.mpl_interface import oplot, oplotr, oploti, plt     
+from pytriqs.gf import Gf
+from pytriqs.gf import inverse
+from pytriqs.gf import iOmega_n
+from pytriqs.gf import MeshImFreq
+from pytriqs.gf import MeshBrillouinZone
+
 from pytriqs.lattice import BrillouinZone, BravaisLattice
 
 # ----------------------------------------------------------------------
 
-from triqs_tprf.ParameterCollection import ParameterCollection
-
-from triqs_tprf.analytic_hubbard_atom import analytic_hubbard_atom
+#from triqs_tprf.ParameterCollection import ParameterCollection
 
 # ----------------------------------------------------------------------
 
-from triqs_tprf.freq_conv import block_iw_AB_to_matrix_valued
-from triqs_tprf.freq_conv import from_3nu_PH
-
-from triqs_tprf.chi_from_gg2 import chi0_from_gg2_PH
-from triqs_tprf.chi_from_gg2 import chi_from_gg2_PH
-
-from triqs_tprf.linalg import inverse_PH
-
-# ----------------------------------------------------------------------
-
-from triqs_tprf.lattice import g0k_from_ek
-from triqs_tprf.lattice import gk_from_ek_sigma
-from triqs_tprf.lattice import gr_from_gk
+from triqs_tprf.lattice import lattice_dyson_g_wk
+from triqs_tprf.lattice import fourier_wk_to_wr
 
 from triqs_tprf.lattice import chi0r_from_gr_PH
 from triqs_tprf.lattice import chi0q_from_chi0r
@@ -40,42 +30,9 @@ from triqs_tprf.lattice import chiq_from_chi0q_and_gamma_PH
 from triqs_tprf.lattice import chiq_sum_nu, chiq_sum_nu_q
 
 # ----------------------------------------------------------------------
-def setup_dispersion_and_bz(parm):
-
-    """Ugly hack for evaluating the dispersion of a tight binding lattice
-    on a MeshBrillouinZone Green's function mesh."""
-
-    nk = 1
-    print 'nk =', nk
-    
-    # -- Get the bravais lattice from parm and constr the Brillouin zone
-
-    bz = BrillouinZone(BravaisLattice([(1,0,0), (0,1,0)]))
-    
-    bzmesh = MeshBrillouinZone(bz, n_k=nk)
-    q_list = np.array([q.value[:2] for q in bzmesh])
-    q_list /= 2.*np.pi
-
-    #for qidx, q in enumerate(bzmesh):
-    #    print qidx, q.value/(2.*np.pi)
-
-    # ------------------------------------------------------------------
-    # -- Setup dispersion
-
-    # using zero dispersion, to get atomic limit
-    
-    ek = Gf(mesh=bzmesh, target_shape=[1, 1])
-
-    for kidx, k in enumerate(bzmesh):
-        k_val = k.value[:, None] / (2. * np.pi)
-        kidx_0 = 0
-        ek.data[kidx, :, :] = 0.0
-
-    nk_tot = len(bzmesh)
-    print 'nk_tot =', nk_tot
-    
-    return ek, bz, nk_tot
         
+from triqs_tprf.analytic_hubbard_atom import analytic_hubbard_atom
+
 # ----------------------------------------------------------------------
 def solve_lattice_bse(parm, momsus=False):
 
@@ -87,7 +44,13 @@ def solve_lattice_bse(parm, momsus=False):
     # ------------------------------------------------------------------
     # -- Setup lattice
 
-    ek, bz, parm.nk = setup_dispersion_and_bz(parm)
+    bl = BravaisLattice([(1,0,0), (0,1,0)])
+
+    bz = BrillouinZone(bl)    
+    bzmesh = MeshBrillouinZone(bz, n_k=1) # only one k-point
+    
+    e_k = Gf(mesh=bzmesh, target_shape=[1, 1])
+    e_k *= 0.
           
     # ------------------------------------------------------------------
     # -- Lattice single-particle Green's function
@@ -101,43 +64,14 @@ def solve_lattice_bse(parm, momsus=False):
     parm.Sigma_iw << inverse(G0_iw) - inverse(parm.G_iw)
     
     parm.mu = 0.5*parm.U
-    gk = gk_from_ek_sigma(mu=parm.mu, ek=ek, sigma=parm.Sigma_iw)
-    gr = gr_from_gk(gk)    
-
-    # ------------------------------------------------------------------
-    # -- check that G_imp == G_loc
-
-    # -- Get the 0,0,0 real space component of the Green's function
-
-    if False:
-        # Complicated way
-        rmesh = gr.mesh.components[1]
-        r_mesh_points = np.array([r for r in rmesh])
-        ridx = rmesh.index_to_linear((0,0,0))
-        r_mesh_point = r_mesh_points[ridx]
-        G_iw = gr[:, r_mesh_point]
-    else:
-        # (Less) Complicated way, thanks Nils Wentzell! :)
-        rmesh = gr.mesh.components[1]
-        ridx = rmesh.index_to_linear((0,0,0))
-        G_iw = gr[:, MeshPoint(ridx)]
-    
-    #oplot(parm.G_iw)
-    #oplot(G_iw)    
-    #plt.show()
-
-    if False:
-        diff = G_iw.data - parm.G_iw.data
-        print '--> Checking G_imp == G_loc,',
-        print 'max|diff| =', np.max(np.abs(diff))
-
-    np.testing.assert_array_almost_equal(G_iw.data, parm.G_iw.data)
+    g_wk = lattice_dyson_g_wk(mu=parm.mu, e_k=e_k, sigma_w=parm.Sigma_iw)
+    g_wr = fourier_wk_to_wr(g_wk)
 
     # ------------------------------------------------------------------
     # -- Non-interacting generalized lattice susceptibility
 
     chi0r = chi0r_from_gr_PH(nw=parm.nw, nnu=parm.nwf, gr=gr) 
-    chi0q = chi0q_from_chi0r(chi0r, bz)
+    chi0q = chi0q_from_chi0r(chi0r)
 
     # ------------------------------------------------------------------
     # -- Solve lattice BSE
@@ -160,25 +94,21 @@ def solve_lattice_bse(parm, momsus=False):
 # ----------------------------------------------------------------------
 if __name__ == '__main__':
 
-    beta = 1.234
-    nwf = 248
-    
-    p = ParameterCollection(
-        beta = beta,
+    p = analytic_hubbard_atom(
+        beta = 1.234,
         U = 5.0,
         nw = 1,
-        nwf = nwf,
-        nwf_gf = 2*nwf,
+        nwf = 248,
+        nwf_gf = 2 * 248,
         )
-
-    p = analytic_hubbard_atom(**p.dict())
+    
     p = solve_lattice_bse(p)
 
     # -- Tracing the analytic generalized susceptibility
     p.chi_w_analytic = np.sum(p.chi_m.data) / p.beta**2    
 
     # -- Tracing the numerical generalized susceptibility
-    p.chi_w_ref = np.sum(p.chiq.data) / p.beta**2 / p.nk
+    p.chi_w_ref = np.sum(p.chiq.data) / p.beta**2
 
     print 'chi_w_analytic =', p.chi_w_analytic
     print 'chi_w.data     =', p.chi_w.data.flatten()
