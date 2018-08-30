3bab93a65d333d00c66110c0d8f027bc0c6335f3
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-08-29 20:25:51 -0400

    [hybrid] mpi+openmp iteration on meshes

--- test/c++/mpi_and_openmp.cpp
+++ test/c++/mpi_and_openmp.cpp
@@ -1,4 +1,6 @@
 
+#include <omp.h>
+
 #include <triqs/test_tools/gfs.hpp>
 
 using namespace triqs::clef;
@@ -10,10 +12,10 @@ using namespace tprf;
 
 // ------------------------------------------------------------
 
-TEST(Gf, Bubble) {
+TEST(mpi, mpi_view) {
 
   triqs::mpi::communicator c;
-  
+
   double beta = 20;
   int n_k = 2;
   int nw = 3;
@@ -23,14 +25,14 @@ TEST(Gf, Bubble) {
       {{beta, Fermion, nw}, {bz, n_k}}, {1, 1}};
 
   auto [wmesh, kmesh] = g_wk.mesh();
-  
+
   // loop over frequency and momentum in mpi
 
   c.barrier();
 
   for (auto const &[w, k] : mpi_view(g_wk.mesh())) {
-    std::cout << "rank " << c.rank() << " size " << c.size() << " : " << w.linear_index()
-              << ", " << k.linear_index() << std::endl;
+    std::cout << "rank " << c.rank() << " size " << c.size() << " : "
+              << w.linear_index() << ", " << k.linear_index() << std::endl;
   }
 
   // loop over frequency in mpi
@@ -38,7 +40,8 @@ TEST(Gf, Bubble) {
   c.barrier();
 
   for (auto const &w : mpi_view(wmesh)) {
-    std::cout << "rank " << c.rank() << " size " << c.size() << " : " << w.linear_index() << std::endl;
+    std::cout << "rank " << c.rank() << " size " << c.size() << " : "
+              << w.linear_index() << std::endl;
   }
 
   // loop over momentum in mpi
@@ -46,12 +49,79 @@ TEST(Gf, Bubble) {
   c.barrier();
 
   for (auto const &k : mpi_view(kmesh)) {
-    std::cout << "rank " << c.rank() << " size " << c.size() << " : " << k.linear_index() << std::endl;
+    std::cout << "rank " << c.rank() << " size " << c.size() << " : "
+              << k.linear_index() << std::endl;
   }
-  
+
   c.barrier();
 }
 
 // ------------------------------------------------------------
+
+TEST(mpi, mpi_view_openmp) {
+
+  triqs::mpi::communicator c;
+
+  double beta = 20;
+  int n_k = 2;
+  int nw = 3;
+
+  auto bz = brillouin_zone{bravais_lattice{{{1, 0}, {0, 1}}}};
+  auto g_wk = gf<cartesian_product<imfreq, brillouin_zone>>{
+      {{beta, Fermion, nw}, {bz, n_k}}, {1, 1}};
+
+  auto [wmesh, kmesh] = g_wk.mesh();
+
+  // loop over frequency and momentum in mpi + openmp
+
+  c.barrier();
+
+  {
+    auto arr = mpi_view(g_wk.mesh());
+#pragma omp parallel for
+    for (int idx = 0; idx < arr.size(); idx++) {
+      auto &[w, k] = arr(idx);
+
+      int tid = omp_get_thread_num();
+      std::cout << "thread " << tid << " rank " << c.rank() << " size "
+                << c.size() << " : " << w.linear_index() << ", "
+                << k.linear_index() << std::endl;
+    }
+  }
+
+  // loop over frequency in mpi + openmp
+
+  c.barrier();
+
+  {
+    auto arr = mpi_view(wmesh);
+#pragma omp parallel for
+    for (int idx = 0; idx < arr.size(); idx++) {
+      auto &w = arr(idx);
+
+      int tid = omp_get_thread_num();
+      std::cout << "thread " << tid << " rank " << c.rank() << " size "
+                << c.size() << " : " << w.linear_index() << std::endl;
+    }
+  }
+
+  // loop over momentum in mpi + openmp
+
+  c.barrier();
+
+  {
+    auto arr = mpi_view(kmesh);
+#pragma omp parallel for
+    for (int idx = 0; idx < arr.size(); idx++) {
+      auto &k = arr(idx);
+
+      int tid = omp_get_thread_num();
+      std::cout << "thread " << tid << " rank " << c.rank() << " size "
+                << c.size() << " : " << k.linear_index() << std::endl;
+    }
+  }
+
+  c.barrier();
+}
 
 MAKE_MAIN;
