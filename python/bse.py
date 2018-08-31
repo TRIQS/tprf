3a88871754a33073075722cb6f71b5034f4690cf
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-08-31 17:10:53 -0400

    [bse] python memory mgmnt and window funct

--- python/bse.py
+++ python/bse.py
@@ -4,6 +4,7 @@ import numpy as np
 # ----------------------------------------------------------------------
 
 import pytriqs.utility.mpi as mpi
+from pytriqs.gf import MeshImFreq, MeshProduct, Gf, Idx
 
 # ----------------------------------------------------------------------
 
@@ -25,6 +26,26 @@ def solve_local_bse(chi0_wnn, chi_wnn):
     return gamma_wnn
 
 # ----------------------------------------------------------------------
+def fixed_fermionic_window_python_wnk(g2, nwf):
+
+    #nw = (g2.data.shape[0] + 1) / 2
+
+    wmesh, nmesh, kmesh = g2.mesh.components
+    
+    beta = g2.mesh.components[0].beta
+    nmesh_small = MeshImFreq(beta=beta, S='Fermion', n_max=nwf)
+
+    g2_out = Gf(mesh=MeshProduct(wmesh, nmesh_small, kmesh), target_shape=g2.target_shape)
+
+    n = g2.data.shape[1]
+    s = n/2 - nwf
+    e = n/2 + nwf
+    
+    g2_out.data[:] = g2.data[:, s:e, :]
+
+    return g2_out
+
+# ----------------------------------------------------------------------
 def get_chi0_wnk(g_wk, nw=1, nwf=None):
 
     fmesh = g_wk.mesh.components[0]
@@ -32,15 +53,15 @@ def get_chi0_wnk(g_wk, nw=1, nwf=None):
     if nwf is None:
         nwf = len(fmesh) / 2
 
-    print '--> g_wr from g_wk'
+    mpi.report('--> g_wr from g_wk')
     g_wr = fourier_wk_to_wr(g_wk)
 
-    print '--> chi0_wnr from g_wr'
+    mpi.report('--> chi0_wnr from g_wr')
     chi0_wnr = chi0r_from_gr_PH(nw=nw, nnu=nwf, gr=g_wr)
 
     del g_wr
 
-    print '--> chi0_wnk from chi0_wnr'
+    mpi.report('--> chi0_wnk from chi0_wnr')
     chi0_wnk = chi0q_from_chi0r(chi0_wnr)
 
     del chi0_wnr
@@ -48,7 +69,7 @@ def get_chi0_wnk(g_wk, nw=1, nwf=None):
     return chi0_wnk    
         
 # ----------------------------------------------------------------------
-def solve_lattice_bse(g_wk, gamma_wnn, tail_corr_nwf=None, return_chi0_wk=False):
+def solve_lattice_bse(g_wk, gamma_wnn, tail_corr_nwf=None):
 
     kmesh = g_wk.mesh.components[1]
     bmesh = gamma_wnn.mesh.components[0]
@@ -58,60 +79,60 @@ def solve_lattice_bse(g_wk, gamma_wnn, tail_corr_nwf=None, return_chi0_wk=False)
     nw = (len(bmesh) + 1) / 2
     nwf = len(fmesh) / 2
 
-    print tprf_banner(), "\n"
+    if mpi.is_master_node():
+        print tprf_banner(), "\n"
+        print 'Lattcie BSE with local vertex approximation.\n'
+        print 'nk  = ', nk
+        print 'nw  = ', nw
+        print 'nwf = ', nwf
+        print    
 
-    print 'Lattcie BSE with local vertex approximation.\n'
-    print 'nk  = ', nk
-    print 'nw  = ', nw
-    print 'nwf = ', nwf
-    print    
-
-    chi0_wnk = get_chi0_wnk(g_wk, nw=nw, nwf=nwf)
-    
-    assert( chi0_wnk.mesh.components[0] == bmesh )
-    assert( chi0_wnk.mesh.components[1] == fmesh )
-    assert( chi0_wnk.mesh.components[2] == kmesh )
-
-    # -- Trace chi0_kw to get correction to chi_kw
-
-    print '--> trace chi0_wnk (tail corr)'
     if tail_corr_nwf is None:
-        chi0_wk_tail_corr = chi0q_sum_nu_tail_corr_PH(chi0_wnk)
-    else:
-        chi0_wnk_tail_corr = get_chi0_wnk(g_wk, nw=nw, nwf=tail_corr_nwf)
-        print '--> chi0q_sum_nu_tail_corr_PH'
-        chi0_wk_tail_corr = chi0q_sum_nu_tail_corr_PH(chi0_wnk_tail_corr)
+        tail_corr_nwf = nwf
 
-    print '--> trace chi0_wnk'
+    mpi.report('--> chi0_wnk_tail_corr')
+    chi0_wnk_tail_corr = get_chi0_wnk(g_wk, nw=nw, nwf=tail_corr_nwf)
+
+    mpi.report('--> trace chi0_wnk_tail_corr')
+    chi0_wk_tail_corr = chi0q_sum_nu_tail_corr_PH(chi0_wnk_tail_corr)
+
+    mpi.report('--> chi0_wnk_tail_corr to chi0_wnk')
+    if tail_corr_nwf != nwf:
+        mpi.report('--> fixed_fermionic_window_python_wnk')
+        chi0_wnk = fixed_fermionic_window_python_wnk(chi0_wnk_tail_corr, nwf=nwf)
+    else:
+        chi0_wnk = chi0_wnk_tail_corr.copy()
+
+    del chi0_wnk_tail_corr
+
+    mpi.report('--> trace chi0_wnk')
     chi0_wk = chi0q_sum_nu(chi0_wnk)
 
     dchi_wk = chi0_wk_tail_corr - chi0_wk
 
     del chi0_wk
+    del chi0_wk_tail_corr
 
-    if not return_chi0_wk:
-        del chi0_wk_tail_corr
+    assert( chi0_wnk.mesh.components[0] == bmesh )
+    assert( chi0_wnk.mesh.components[1] == fmesh )
+    assert( chi0_wnk.mesh.components[2] == kmesh )
 
-    if tail_corr_nwf is not None:
-        del chi0_wnk_tail_corr
-    
     # -- Lattice BSE calc with built in trace
-    print '--> chi_kw from BSE'
+    mpi.report('--> chi_kw from BSE')
     chi_kw = chiq_sum_nu_from_chi0q_and_gamma_PH(chi0_wnk, gamma_wnn)
 
     del chi0_wnk
 
-    print '--> chi_kw tail corrected (using chi0_wnk)'
+    mpi.report('--> chi_kw tail corrected (using chi0_wnk)')
     for k in chi_kw.mesh.components[0]:
         chi_kw[k, :] += dchi_wk[:, k] # -- account for high freq of chi_0 (better than nothing)
 
     del dchi_wk
 
-    if return_chi0_wk:
-        return chi_kw, chi0_wk_tail_corr
-    else:
-        return chi_kw
+    mpi.report('--> solve_lattice_bse, done.')
 
+    return chi_kw
+ 
 # ----------------------------------------------------------------------
 def solve_lattice_bse_depr(g_wk, gamma_wnn, tail_corr_nwf=-1):
 
