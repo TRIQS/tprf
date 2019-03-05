2259e36acc208dad10651ec2307af52f4c11f9db
Hugo U. R. Strand <hugo.strand@gmail.com>
2019-03-04 22:04:34 -0500

    [rpa] charge spin diag of general interaction

--- python/rpa_tensor.py
+++ python/rpa_tensor.py
@@ -109,3 +109,110 @@ def get_rpa_uc_tensor(norbs, U, Up, J, Jp):
 
     return U_abcd
 
+# ----------------------------------------------------------------------    
+def kanamori_charge_and_spin_quartic_interaction_tensors(norb, U, Up, J, Jp):
+
+    """ Following Eliashberg notes, but with c+c+ cc order. """
+
+    shape = [norb]*4
+    U_c, U_s = np.zeros(shape), np.zeros(shape)
+    
+    for a, abar, b, bbar in itertools.product(range(norb), repeat=4):
+
+        scalar_c, scalar_s = 0, 0
+
+        if (a == abar) and (a == b) and (b == bbar):
+            scalar_c = U
+            scalar_s = U
+            
+        if (a == bbar) and  (a != b) and (abar == b):
+            scalar_c = -Up + 2*J
+            scalar_s = Up
+
+        if (a == abar) and  (a != b) and (b == bbar):
+            scalar_c = 2*Up - J
+            scalar_s = J
+
+        if (a == b) and  (a != abar) and (abar == bbar):
+            scalar_c = Jp
+            scalar_s = Jp
+            
+        U_c[a, b, bbar, abar] = scalar_c
+        U_s[a, b, bbar, abar] = scalar_s
+
+    return U_c, U_s
+
+# ----------------------------------------------------------------------    
+def split_quartic_tensor_in_charge_and_spin(U_4):
+
+    """Assuming spin is the slow index and orbital is the fast index 
+
+    Using a rank 4 U_abcd tensor with composite (spin, orbital) indices
+    as input, assuming c^+c^+ c c structure of the tensor
+
+    Returns:
+
+    U_c : Charge channel rank 4 interaction tensor
+    U_s : Spin channel rank 4 interaction tensor """
+
+    shape_4 = np.array(U_4.shape)
+    shape_8 = np.vstack(([2]*4, shape_4 / 2)).T.flatten()
+
+    U_8 = U_4.reshape(shape_8)
+
+    U_8 = np.transpose(U_8, (0, 2, 4, 6, 1, 3, 5, 7)) # spin first
+
+    # -- Check spin-conservation
+
+    zeros = np.zeros_like(U_8[0, 0, 0, 0])
+    
+    np.testing.assert_array_almost_equal(U_8[0, 0, 0, 1], zeros)
+    np.testing.assert_array_almost_equal(U_8[0, 0, 1, 0], zeros)
+    np.testing.assert_array_almost_equal(U_8[0, 1, 0, 0], zeros)
+    np.testing.assert_array_almost_equal(U_8[1, 0, 0, 0], zeros)
+
+    np.testing.assert_array_almost_equal(U_8[1, 1, 1, 0], zeros)
+    np.testing.assert_array_almost_equal(U_8[1, 1, 0, 1], zeros)
+    np.testing.assert_array_almost_equal(U_8[1, 0, 1, 1], zeros)
+    np.testing.assert_array_almost_equal(U_8[0, 1, 1, 1], zeros)
+
+    np.testing.assert_array_almost_equal(U_8[0, 0, 1, 1], zeros)
+    np.testing.assert_array_almost_equal(U_8[1, 1, 0, 0], zeros)
+
+    # -- split in charge and spin
+    
+    # c+ c c+ c form of the charge, spin diagonalization
+    #U_c = U_8[0, 0, 0, 0] + U_8[0, 0, 1, 1]
+    #U_s = U_8[0, 0, 0, 0] - U_8[0, 0, 1, 1]
+
+    # c+ c+ c c  form of the charge, spin diagonalization
+    U_c = U_8[0, 0, 0, 0] + U_8[0, 1, 1, 0]
+    U_s = -U_8[0, 0, 0, 0] + U_8[0, 1, 1, 0]
+
+    U_c *= 4
+    U_s *= 4
+    
+    return U_c, U_s
+
+# ----------------------------------------------------------------------    
+def quartic_tensor_from_charge_and_spin(U_c, U_s):
+
+    shape_4 = U_c.shape
+    shape_8 = [2]*4 + list(shape_4)
+
+    U_8 = np.zeros(shape_8, dtype=U_c.dtype)
+
+    U_uu = 0.5 * (U_c - U_s)
+    U_ud = 0.5 * (U_c + U_s)
+    
+    for s1, s2 in itertools.product(range(2), repeat=2):
+        if s1 == s2:
+            U_8[s1, s1, s1, s1] = U_uu
+        else:
+            U_8[s1, s2, s2, s1] = U_ud
+            U_8[s1, s2, s1, s2] = -U_s
+
+    U_8 = np.transpose(U_8, (0, 4, 1, 5, 2, 6, 3, 7))
+    U_4 = U_8.reshape(2*np.array(shape_4))
+    
+    return 0.25*U_4
---
+++ test/python/interaction_tensor_charge_spin_factorization.py
@@ -0,0 +1,64 @@
+
+import numpy as np
+
+# ----------------------------------------------------------------------    
+
+from pytriqs.operators import Operator
+from pytriqs.operators.util.U_matrix import U_matrix_kanamori
+from pytriqs.operators.util.hamiltonians import h_int_kanamori
+from pytriqs.operators.util.op_struct import set_operator_structure
+
+# ----------------------------------------------------------------------    
+
+from pyed.OperatorUtils import quartic_permutation_symmetrize
+from pyed.OperatorUtils import fundamental_operators_from_gf_struct
+from pyed.OperatorUtils import get_quadratic_operator,  quadratic_matrix_from_operator
+from pyed.OperatorUtils import quartic_tensor_from_operator, operator_from_quartic_tensor
+from pyed.OperatorUtils import operator_single_particle_transform, relabel_operators
+
+# ----------------------------------------------------------------------    
+
+from triqs_tprf.rpa_tensor import kanamori_charge_and_spin_quartic_interaction_tensors
+from triqs_tprf.rpa_tensor import split_quartic_tensor_in_charge_and_spin
+from triqs_tprf.rpa_tensor import quartic_tensor_from_charge_and_spin
+
+# ----------------------------------------------------------------------    
+if __name__ == '__main__':
+
+    U = 1.0
+    J = 0.3
+    
+    orb_names = ['xy', 'xz', 'yz']
+    spin_names = ['up', 'do']
+    norb = len(orb_names)
+
+    gf_struct = set_operator_structure(spin_names, orb_names, True) # orbital off diag
+    fundamental_operators = fundamental_operators_from_gf_struct(gf_struct)
+    
+    U_ab, UPrime_ab = U_matrix_kanamori(
+        n_orb=len(orb_names), U_int=U, J_hund=J)
+    
+    H_int = h_int_kanamori(
+        spin_names, orb_names, U_ab, UPrime_ab, J_hund=J,
+        #off_diag=False, map_operator_structure=None, H_dump=None) # orbital diag
+        off_diag=True, map_operator_structure=None, H_dump=None) # orgital offdiag
+
+    U_abcd = quartic_tensor_from_operator(H_int, fundamental_operators, perm_sym=True)
+
+    # -- test
+    H_int_ref = operator_from_quartic_tensor(U_abcd, fundamental_operators)
+    assert( H_int - H_int_ref == Operator(0.))
+
+    # -- charge and spin interaction tensors
+
+    U_c, U_s = split_quartic_tensor_in_charge_and_spin(U_abcd)
+
+    U_abcd_ref = quartic_tensor_from_charge_and_spin(U_c, U_s)
+    np.testing.assert_array_almost_equal(U_abcd, U_abcd_ref)
+
+    U_c_ref, U_s_ref = kanamori_charge_and_spin_quartic_interaction_tensors(
+        norb, U, U - 2*J, J, J)
+
+    np.testing.assert_array_almost_equal(U_c, U_c_ref)
+    np.testing.assert_array_almost_equal(U_s, U_s_ref)
+    
