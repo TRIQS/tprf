dc8ef30c25a4bdb1692247bc9755575b363acb6c
Stefan <stefan.kaeser7@gmail.com>
2019-04-02 13:34:49 +0200

    [benchmark] improve memory analysing scripts

--- benchmark/memory/observe_memory.py
+++ benchmark/memory/observe_memory.py
@@ -4,15 +4,14 @@ import subprocess
 
 import numpy as np
 
-filename = sys.argv.pop()
-
-duration, it = [float(ele) for ele in sys.argv[1:]]
+filename = sys.argv[-1]
+it = float(sys.argv[1])
 
 bash_command = "free -m | grep Mem >> %s"%filename
 
 subprocess.call(['bash', '-c', "free - m | grep total > %s"%filename])
 
-for t in np.arange(0, duration, it):
+while True:
     time.sleep(it)
-    print("%.2f"%t)
+    #print("%.2f"%t)
     subprocess.call(['bash', '-c', bash_command])
