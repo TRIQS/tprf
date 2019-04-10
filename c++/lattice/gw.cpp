a75fe49b5d414df6e6868fffcc1ba8069e18d8a7
Hugo U. R. Strand <hugo.strand@gmail.com>
2019-04-10 11:01:50 -0400

    [gw] sigma

--- c++/lattice/gw.cpp
+++ c++/lattice/gw.cpp
@@ -37,23 +37,48 @@ chi_wk_t screened_interaction_W(chi_wk_vt PI_wk, chi_k_vt V_k) {
     array<scalar_t, 4> t_arr{t, memory_layout_t<4>{0, 1, 2, 3}};
     auto t_mat = make_matrix_view(group_indices_view(t_arr, {0, 1}, {3, 2}));
     return t_mat;
-    };
+  };
 
   // MPI and openMP parallell loop
   auto arr = mpi_view(W_wk.mesh());
 #pragma omp parallel for
   for (int idx = 0; idx < arr.size(); idx++) {
-    auto &[w, k] = arr(idx);  
+    auto &[w, k] = arr(idx);
 
-    auto PI = to_matrix(PI_wk[w, k]);
-    auto W = to_matrix(W_wk[w, k]);
     auto V = to_matrix(V_k[k]);
+    auto PI = to_matrix(PI_wk[w, k]);
+
+    // auto W = to_matrix(W_wk[w, k]);
+
+    array<scalar_t, 4> W_arr{nb, nb, nb, nb, memory_layout_t<4>{0, 1, 2, 3}};
+    auto W = make_matrix_view(group_indices_view(W_arr, {0, 1}, {3, 2}));
 
     W = V * inverse(I - PI * V);
+
+    W_wk[w, k] = W_arr;
   }
 
   W_wk = mpi_all_reduce(W_wk);
   return W_wk;
+}
+
+g_wk_t gw_self_energy(chi_wk_vt W_wk, g_wk_vt g_wk) {
+
+  auto g_tr = make_gf_from_fourier<0, 1>(g_wk);
+  auto W_tr = make_gf_from_fourier<0, 1>(W_wk);
+
+  auto sigma_tr = make_gf(g_tr);
+  sigma_tr *= 0.;
+
+  for (const auto &[t, r] : g_tr.mesh()) {
+    for (const auto &[a, b, c, d] : W_tr.target_indices()) {
+      sigma_tr[t, r](a, b) += W_tr[t, r](a, b, c, d) * g_tr[t, r](c, d);
+    }
+  }
+
+  auto sigma_wk = make_gf_from_fourier<0, 1>(sigma_tr);
+
+  return sigma_wk;
 }
 
 } // namespace tprf
--- c++/lattice/gw.hpp
+++ c++/lattice/gw.hpp
@@ -41,5 +41,37 @@ namespace tprf {
  */
 
 chi_wk_t screened_interaction_W(chi_wk_vt PI_wk, chi_k_vt V_k);
-  
+
+/** GW self energy :math:`\Sigma(i\omega_n, \mathbf{k})` calculator 
+
+    Fourier transforms the screened interaction and the single-particle
+    Green's function to imagiary time and real space.
+
+    .. math::
+        G_{ab}(\tau, \mathbf{r}) = \mathcal{F}^{-1}
+          \left\{ G_{ab}(i\omega_n, \mathbf{k}) \right\}
+
+    .. math::
+        W_{abcd}(\tau, \mathbf{r}) = \mathcal{F}^{-1}
+          \left\{ W_{abcd}(i\omega_n, \mathbf{k}) \right\}
+
+    computes the GW self-energy as the product
+
+    .. math::
+        \Sigma_{ab}(\tau, \mathbf{r}) =
+          \sum_{cd} W_{abcd}(\tau, \mathbf{r}) G_{cd}(\tau, \mathbf{r})
+
+    and transforms back to frequency and momentum
+
+    .. math::
+        \Sigma_{ab}(i\omega_n, \mathbf{k}) =
+          \mathcal{F} \left\{ \Sigma_{ab}(\tau, \mathbf{r}) \right\}
+
+    @param W_wk screened interaction :math:`W_{abcd}(i\omega_n, \mathbf{k})`
+    @param g_wk single particle Green's function :math:`G_{ab}(i\omega_n, \mathbf{k})`
+    @return GW self-energy :math:`\Sigma_{ab}(i\omega_n, \mathbf{k})`
+ */
+
+g_wk_t gw_self_energy(chi_wk_vt W_wk, g_wk_vt g_wk);
+
 } // namespace tprf
