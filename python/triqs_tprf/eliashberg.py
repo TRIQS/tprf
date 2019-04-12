9e388df4f81366a8f56b399b16907e7c015b210b
Stefan <stefan.kaeser7@gmail.com>
2019-04-12 15:28:36 +0200

    [eli] generalize solving methods
    - Restructure the eliashberg module to be more flexible
    - Add power method
    - Test eliashberg_product and the eigenvalue solver individually in
    seperate tests.
    - Add documentation

--- python/triqs_tprf/eliashberg.py
+++ python/triqs_tprf/eliashberg.py
@@ -28,47 +28,101 @@ from scipy.sparse.linalg import eigs
 
 # ----------------------------------------------------------------------
 
+from pytriqs.gf import Gf
 from lattice import eliashberg_product
 from lattice import eliashberg_product_fft
 from lattice import split_into_dynamic_wk_and_constant_k, dynamic_and_constant_to_tr
 
 # ----------------------------------------------------------------------
-def solve_eliashberg(Gamma_pp, g_wk, tol=1e-10):
 
-    """ Solve the linearized Eliashberg equation using 
-    iterative eigenvalue algorithms from scipy """
-    
-    def from_x_to_wk(delta_x):
-        delta_wk = g_wk.copy()
-        delta_wk.data[:] = delta_x.reshape(delta_wk.data.shape)
-        return delta_wk
+def semi_random_initial_delta(g_wk, nr_factor=0.5, seed=None):
 
-    def from_wk_to_x(delta_wk):
-        delta_x = delta_wk.data.copy().flatten()
-        return delta_x
-    
-    def matvec(delta_x):
-        delta_wk = from_x_to_wk(delta_x)
-        delta_out_wk = eliashberg_product(Gamma_pp, g_wk, delta_wk)
-        delta_out_x = from_wk_to_x(delta_out_wk)
-        return delta_out_x
+    r"""Create a delta based on the GF with random elements
 
-    x = from_wk_to_x(g_wk)
-    N = x.shape[0]
-    linop = LinearOperator(matvec=matvec, dtype=np.complex, shape=(N, N))
+    Returns an anomalous self-energy that can be used as an inital input for the iterative
+    solvers. The momentum space is random, while the Matsubara space is only partialy 
+    randomized to ensure working tail fits for the Fourier transformations.
 
-    np.random.seed(1337)
-    v0 = np.random.random(N)
-    E, U = eigs(linop, which='LR', tol=tol, v0=v0)
+    Parameters
+    ----------
+    g_wk : Gf, 
+           Green's function :math:`G(i\nu_n, \mathbf{k})`. The mesh attribute of the Gf must
+           be a MeshProduct with the components (MeshImFreq, MeshBrillouinZone).
+    nr_factor : float, optional
+                Percentage of :math:`\omega` points which shall not be randomized. This is needed
+                to assure a working tail fit for the Fourier transformations. The default is 0.5,
+                meaning that 50% of the :math:`\omega` points will not be randomized.
+    seed : int, optional
+           Set a np.random.seed to enforce predictable results.
 
-    eigen_modes = []
-    for idx in xrange(U.shape[-1]):
-        delta_wk = from_x_to_wk(U[:, idx])
-        eigen_modes.append(delta_wk)
+    Returns
+    -------
+    delta : Gf,
+            An inital anomalous self-energy :math:`\Delta(i\nu_n, \mathbf{k})` to start
+            a iterative solver, given as a Gf with MeshProduct with the components
+            (MeshImFreq, MeshBrillouinZone).
+    """
 
-    return E, eigen_modes
-    
-def solve_eliashberg_fft(Gamma_pp_wk, g_wk, Gamma_pp_const_k=None, tol=1e-10):
+    np.random.seed(seed)
+
+    delta = g_wk.copy()
+    shape = delta.data.shape
+    delta.data[:] = delta.data.real # Pure real delta is sufficient w/o magnetic field
+    random_data = np.random.random(shape[1:])
+    freq_data = np.mean(np.abs(delta.data), axis=tuple(range(len(shape))[1:]))
+    not_randomized = int(nr_factor*shape[0] / 2.)
+    start, stop = not_randomized, shape[0]-not_randomized
+    freq_data[start:stop] *= np.random.random(stop-start)
+
+    delta.data[:] = np.tensordot(freq_data, random_data, axes=0)
+
+    return delta
+
+def preprocess_gamma_for_fft(Gamma_pp_wk, Gamma_pp_const_k):
+    r""" Prepare Gamma to be used with the FFT implementation
+
+    Parameters
+    ----------
+    Gamma_pp_wk : Gf,
+               Pairing vertex :math:`\Gamma(i\omega_n, \mathbf{k})`. The mesh attribute of
+               the Gf must be a MeshProduct with the components (MeshImFreq, MeshBrillouinZone).
+    Gamma_pp_const_k : float or np.ndarray or Gf
+                       Part of the pairing vertex that is constant in Matsubara frequency space
+                       :math:`\Gamma(\mathbf{k})`. If given as a Gf its mesh attribute needs to
+                       be a MeshBrillouinZone.
+
+    Returns
+    -------
+    Gamma_pp_dyn_tr : Gf,
+                      The dynamic part of Gamma, which converges to zero for
+                      :math:`\omega_n \rightarrow \infty`, but now in :math:`\tau`-space.
+                      Its mesh attribute is MeshProduct with the components
+                      (MeshImTime, MeshCyclicLattice).
+    Gamma_pp_const_r : Gf,
+                       The constant part of Gamma with mesh attribute MeshCyclicLattice.
+    """
+
+    # -- Determine the dynamic and constant part via a tail fit
+    # -- (This is done even if the constant term is given to get the specific Gf types)
+    Gamma_pp_dyn_wk_fit, Gamma_pp_const_k_fit = split_into_dynamic_wk_and_constant_k(Gamma_pp_wk)
+
+    # -- Use a constant term if explicitly given
+    const_type = type(Gamma_pp_const_k)
+    if (const_type == float) or (const_type == np.ndarray):
+        Gamma_pp_const_k_fit.data[:] = Gamma_pp_const_k
+        Gamma_pp_dyn_wk_fit.data[:] = Gamma_pp_wk.data - Gamma_pp_const_k
+    elif (const_type == Gf):
+        Gamma_pp_const_k_fit[:] = Gamma_pp_const_k.data
+        Gamma_pp_dyn_wk_fit.data[:] = Gamma_pp_wk.data - Gamma_pp_const_k.data
+
+    # -- FFT dynamic and constant term to (tau, real) or (real)
+    Gamma_pp_dyn_tr, Gamma_pp_const_r = dynamic_and_constant_to_tr(Gamma_pp_dyn_wk_fit, 
+                                                                    Gamma_pp_const_k_fit)
+
+    return Gamma_pp_dyn_tr, Gamma_pp_const_r
+
+def solve_eliashberg(Gamma_pp_wk, g_wk, Gamma_pp_const_k=None, tol=1e-10,
+                            product='FFT', solver='PM', nr_factor=0.5, seed=None):
 
     r""" Solve the linearized Eliashberg equation
     
@@ -90,6 +144,28 @@ def solve_eliashberg_fft(Gamma_pp_wk, g_wk, Gamma_pp_const_k=None, tol=1e-10):
                        be a MeshBrillouinZone.
     tol : float, optional
           Relative accuracy for eigenvalues (stopping criterion).
+    product : str, ['FFT', 'SUM'], optional
+              Which function of the Eliashberg product shall be used:
+
+                  'FFT' : triqs_tprf.lattice.eliashberg_product_fft,
+                          which uses Fourier transformation for optimal computational efficiency.
+                          Restrictions : 
+                  'SUM' : triqs_tprf.lattice.eliashberg_product, uses the explicit sum.
+                          Restrictions : wmesh of Gamma_pp_wk must be atleast twice the size
+                                         of the one of g_wk.
+    solver : str, ['PM', 'IRAM'], optional
+             Which eigenvalue solver shall be used:
+
+                 'PM' : Use the Power Method implemented in :func:`power_method_LR`.
+
+                 'IRAM' : Use the Implicitly Restarted Arnoldi Method implemented in :func:`implicitly_restarted_arnoldi_method`.
+    nr_factor : float, optional
+                Percentage of :math:`\omega` points of the inital delta, which shall not be
+                randomized. This is needed to assure a working tail fit for the
+                Fourier transformations. The default is 0.5, meaning that 50% of
+                the :math:`\omega` points will not be randomized.
+    seed : int, optional
+           Set a np.random.seed to enforce predictable results.
 
     Returns
     -------
@@ -102,27 +178,9 @@ def solve_eliashberg_fft(Gamma_pp_wk, g_wk, Gamma_pp_const_k=None, tol=1e-10):
 
     See Also
     --------
-    :ref:`eliashberg`
-
+    :ref:`eliashberg` : Theory of the linearized Eliashberg equation.
     """
 
-    # -- Determine the dynamic and constant part via a tail fit
-    # -- (This is done even if the constant term is given to get the specific GF types)
-    Gamma_pp_dyn_wk_fit, Gamma_pp_const_k_fit = split_into_dynamic_wk_and_constant_k(Gamma_pp_wk)
-
-    # -- Use a constant term if explicitly given
-    if Gamma_pp_const_k:
-        try:
-            Gamma_pp_const_k_fit.data[:] = Gamma_pp_const_k
-            Gamma_pp_dyn_wk_fit.data[:] = Gamma_pp_wk.data - Gamma_pp_const_k
-        except TypeError:
-            Gamma_pp_const_k_fit[:] = Gamma_pp_const_k.data
-            Gamma_pp_dyn_wk_fit.data[:] = Gamma_pp_wk.data - Gamma_pp_const_k.data
-
-    # -- FFT dynamic and constant term to (tau, real) or (real)
-    Gamma_pp_dyn_tr, Gamma_pp_const_r = dynamic_and_constant_to_tr(Gamma_pp_dyn_wk_fit, 
-                                                                    Gamma_pp_const_k_fit)
-    
     def from_x_to_wk(delta_x):
         delta_wk = g_wk.copy()
         delta_wk.data[:] = delta_x.reshape(delta_wk.data.shape)
@@ -131,25 +189,140 @@ def solve_eliashberg_fft(Gamma_pp_wk, g_wk, Gamma_pp_const_k=None, tol=1e-10):
     def from_wk_to_x(delta_wk):
         delta_x = delta_wk.data.copy().flatten()
         return delta_x
-    
-    def matvec(delta_x):
-        delta_wk = from_x_to_wk(delta_x)
-        delta_out_wk = eliashberg_product_fft(Gamma_pp_dyn_tr, Gamma_pp_const_r, g_wk, delta_wk)
-        delta_out_x = from_wk_to_x(delta_out_wk)
-        return delta_out_x
 
-    x = from_wk_to_x(g_wk)
-    N = x.shape[0]
+    if product == 'FFT':
+
+        Gamma_pp_dyn_tr, Gamma_pp_const_r = preprocess_gamma_for_fft(Gamma_pp_wk, Gamma_pp_const_k)
+
+        def matvec(delta_x):
+            delta_wk = from_x_to_wk(delta_x)
+            delta_out_wk = eliashberg_product_fft(Gamma_pp_dyn_tr, Gamma_pp_const_r, g_wk, delta_wk)
+            delta_out_x = from_wk_to_x(delta_out_wk)
+            return delta_out_x
+
+    elif product == 'SUM':
+
+        def matvec(delta_x):
+            delta_wk = from_x_to_wk(delta_x)
+            delta_out_wk = eliashberg_product(Gamma_pp_wk, g_wk, delta_wk)
+            delta_out_x = from_wk_to_x(delta_out_wk)
+            return delta_out_x
+
+    else:
+        raise NotImplementedError('There is no implementation of the eliashberg product'
+                                    ' called %s.'%product)
+
+    init = from_wk_to_x(semi_random_initial_delta(g_wk, nr_factor=nr_factor, seed=seed))
+
+    if solver == 'PM':
+        es, evs = power_method_LR(matvec, init, tol=tol)
+        es, evs = [es], [evs]
+
+    elif solver == 'IRAM':
+        es, evs = implicitly_restarted_arnoldi_method(matvec, init, tol=tol)
+
+    else:
+        raise NotImplementedError('There is no solver called %s.'%solver)
+
+    eigen_modes = [from_x_to_wk(ele) for ele in evs]
+
+    return es, eigen_modes
+
+def implicitly_restarted_arnoldi_method(matvec, init, tol=1e-10):
+
+    """Find the eigenvalue with the largest real value via the Implicitly Restarted 
+    Arnoldi Method
+
+    Parameters
+    ----------
+    matvec : callable f(v),
+             Returns A*v.
+    init : np.ndarray,
+           The array representation of the anomalous self-energy to start the iterative
+           method with. Restriction: len(init.shape) == 1.
+    tol : float, optional
+          The tolerance at which the iterative scheme is considered to be converged.
+
+    Returns
+    -------
+    Es : list of float,
+           The eigenvalues with the largest positive real part.
+    U : list of np.ndarray,
+          The corresponding eigenvectors.
+
+    Notes
+    -----
+    `scipy.sparse.linalg.eigs <https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.sparse.linalg.eigs.html>`_
+
+    `scipy.sparse.linalg.LinearOperator <https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.LinearOperator.html>`_
+    """
+    N = init.shape[0]
     linop = LinearOperator(matvec=matvec, dtype=np.complex, shape=(N, N))
-
-    np.random.seed(1337)
-    v0 = np.random.random(N)
-    Es, U = eigs(linop, which='LR', tol=tol, v0=v0)
-
-    eigen_modes = []
-    for idx in xrange(U.shape[-1]):
-        delta_wk = from_x_to_wk(U[:, idx])
-        eigen_modes.append(delta_wk)
-
-    return list(Es), eigen_modes
+    Es, U = eigs(linop, which='LR', tol=tol, v0=init)
+    Es = Es.real
     
+    return list(Es), list(U.T)
+
+def power_method_LR(matvec, init, tol=1e-10, max_it=1e5):
+
+    """Find the eigenvalue with the largest real value via the power method
+
+    Parameters
+    ----------
+    matvec : callable f(v),
+             Returns A*v.
+    init : np.ndarray,
+           The array representation of the anomalous self-energy to start the iterative
+           method with. Restriction: len(init.shape) == 1.
+    tol : float, optional
+          The tolerance at which the iterative scheme is considered to be converged.
+    max_it : float, optional
+             The maximum number of iterations that shall be done before a error is raised.
+
+    Returns
+    -------
+    norm : float,
+           The eigenvalue with the largest positive real part.
+    v_k : np.ndarray,
+          The corresponding eigenvector.
+    """
+
+    def iteration(v_k, offset=0.0):
+        v_k1 = matvec(v_k) - offset*v_k
+        v_k1_norm = np.linalg.norm(v_k1)
+        v_k1 = v_k1 / v_k1_norm
+        return v_k1_norm+offset, v_k1
+
+    def power_method(init, offset=0.0, tol=tol, max_it=max_it):
+        norm, v_k = iteration(init, offset)
+        it = 1
+        while True:
+            norm, new_v_k = iteration(v_k, offset)
+            
+            # -- Convergence criterion
+            add = np.max(np.abs(v_k + new_v_k))
+            diff = np.max(np.abs(v_k - new_v_k))
+
+            if (np.allclose(add, 0, atol=tol)) or (np.allclose(diff, 0, atol=tol)):
+                break
+
+            v_k = new_v_k
+            it += 1
+            if it > max_it:
+                raise AssertionError('Did not converge.')
+        return norm, v_k
+
+    # Find eigenvalue with maximum magnitude
+    norm, v_k = power_method(init, tol=tol)
+
+    # Check sign of found eigenvalue
+    _, v_k_test = iteration(v_k)
+
+    add = np.sum(np.abs(v_k + v_k_test)) # small if sign of E is negative
+    diff = np.sum(np.abs(v_k - v_k_test)) # small if sign of E is positive
+
+    # -- Return eigenvalue with largest real part
+    if diff > add: # The eigenvalue with the largest magnitude is negative
+        norm, v_k = power_method(init, offset=-norm, tol=tol)
+    return norm, v_k
+
---
+++ test/python/eliashberg/product_summation_vs_fft.py
@@ -0,0 +1,219 @@
+# ----------------------------------------------------------------------
+
+""" Compare the summation implementation of the linearized Eliashberg product
+and the one using Fourier transformations.
+"""
+
+# ----------------------------------------------------------------------
+
+import itertools
+
+# ----------------------------------------------------------------------
+
+import numpy as np
+
+# ----------------------------------------------------------------------
+
+from triqs_tprf.ParameterCollection import ParameterCollection
+from pytriqs.gf import Gf, MeshImFreq, Idx
+
+from triqs_tprf.tight_binding import TBLattice
+
+from triqs_tprf.lattice import lattice_dyson_g0_wk, solve_rpa_PH
+from triqs_tprf.lattice_utils import imtime_bubble_chi0_wk
+from triqs_tprf.rpa_tensor import kanamori_charge_and_spin_quartic_interaction_tensors
+from triqs_tprf.lattice import gamma_PP_singlet
+from triqs_tprf.lattice import eliashberg_product, eliashberg_product_fft
+from triqs_tprf.eliashberg import semi_random_initial_delta, preprocess_gamma_for_fft
+
+# ----------------------------------------------------------------------
+
+def compare_deltas(deltas_1, deltas_2=None, static=False):
+    """ Build comparison matrix of list of Gf
+    """
+
+    if not deltas_2:
+        deltas_2 = deltas_1
+
+    if static:
+        deltas_1 = [ele[Idx(0), :] for ele in deltas_1]
+        deltas_2 = [ele[Idx(0), :] for ele in deltas_2]
+
+    diff = np.zeros(shape=(len(deltas_1), len(deltas_2)))
+
+    for i, delta_1 in enumerate(deltas_1):
+        for j, delta_2 in enumerate(deltas_2):
+
+            diff[i,j] = np.max(np.abs(delta_1.data - delta_2.data))
+
+    return diff
+
+def print_diff(diff):
+    """ Print output of 'compare_deltas' more readable
+    """
+
+    i_max, j_max = diff.shape
+
+    s = ""
+    s += "\n Differences matrix\n"
+    dashes = "-"*14*diff.shape[0] + "\n"
+    s += dashes
+
+    for i in range(i_max):
+
+        for j in range(j_max):
+
+            s += np.format_float_scientific(diff[i,j], precision=2, pad_left=3)
+            s += "\t"
+
+        s += "\n"
+    s += dashes
+    print(s)
+
+def compare_next_delta(p):
+
+    # -- Setup model, RPA susceptibilities and spin/charge interaction
+
+    full_units = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+    all_nn_hoppings = list(itertools.product([-1, 0, 1], repeat=p.dim)) 
+    non_diagonal_hoppings = [ele for ele in all_nn_hoppings if sum(np.abs(ele)) == 1] 
+
+    t = -p.t * np.eye(p.norbs)
+
+    H = TBLattice(
+                units = full_units[:p.dim],
+                hopping = {hop : t for hop in non_diagonal_hoppings},
+                orbital_positions = [(0,0,0)]*p.norbs,
+                )
+
+    e_k = H.on_mesh_brillouin_zone(n_k=[p.nk]*p.dim + [1]*(3-p.dim))
+
+    # A bigger w-mesh is needed to construct a Gamma with a twice as big w-mesh than GF
+
+    wmesh = MeshImFreq(beta=p.beta, S='Fermion', n_max=p.nw)
+    wmesh_big = MeshImFreq(beta=p.beta, S='Fermion', n_max=int(p.big_factor*p.nw)+1)
+
+    g0_wk = lattice_dyson_g0_wk(mu=p.mu, e_k=e_k, mesh=wmesh)
+    g0_wk_big = lattice_dyson_g0_wk(mu=p.mu, e_k=e_k, mesh=wmesh_big)
+
+    chi0_wk = imtime_bubble_chi0_wk(g0_wk, nw=p.nw)
+    chi0_wk_big = imtime_bubble_chi0_wk(g0_wk_big, nw=int(p.big_factor*p.nw)+1)
+
+    U_c, U_s = kanamori_charge_and_spin_quartic_interaction_tensors(p.norbs, p.U, p.Up, p.J,p.Jp)
+
+    chi_s = solve_rpa_PH(chi0_wk, U_s)
+    chi_c = solve_rpa_PH(chi0_wk, -U_c) # Minus for correct charge rpa equation
+    chi_s_big = solve_rpa_PH(chi0_wk_big, U_s)
+    chi_c_big = solve_rpa_PH(chi0_wk_big, -U_c) # Minus for correct charge rpa equation
+
+    gamma = gamma_PP_singlet(chi_c, chi_s, U_c, U_s)
+    gamma_big = gamma_PP_singlet(chi_c_big, chi_s_big, U_c, U_s)
+
+    # -- Preprocess gamma for the FFT implementations
+
+
+    if p.fit_const:
+        gamma_dyn_tr, gamma_const_r = preprocess_gamma_for_fft(gamma, None)
+    else:
+        gamma_dyn_tr, gamma_const_r = preprocess_gamma_for_fft(gamma, 0.5*(U_s + U_c))
+
+    # -- Creating Semi-Random input Delta
+
+    v0 = semi_random_initial_delta(g0_wk, nr_factor=p.nr_factor, seed=1337)
+    p.v0 = v0
+
+    # -- Test the Eliashberg product
+    
+    print('Start the summation')
+    next_delta = eliashberg_product(gamma_big, g0_wk, p.v0)
+    print('Start the FFT')
+    next_delta_fft = eliashberg_product_fft(gamma_dyn_tr, gamma_const_r, g0_wk, p.v0)
+
+    from pytriqs.plot.mpl_interface import oplot, plt
+    import warnings 
+    warnings.filterwarnings("ignore") #ignore some matplotlib warnings
+    subp = [4, 3, 1]
+    fig = plt.figure(figsize=(18, 15))
+
+    deltas = [v0, next_delta, next_delta_fft]
+    titles = ['Input', 'Summation', 'FFT']
+
+    for k_point in [Idx(0,0,0), Idx(1,0,0)]:
+
+        ax = plt.subplot(*subp); subp[-1] += 1
+        oplot(g0_wk[:, k_point])
+        plt.title('GF')
+
+        ax = plt.subplot(*subp); subp[-1] += 1
+        oplot(gamma[:, k_point])
+        plt.title('Gamma')
+
+        ax = plt.subplot(*subp); subp[-1] += 1
+        oplot(gamma_dyn_tr[:, k_point])
+        plt.title('Gamma dyn tr')
+
+        for delta, title in zip(deltas, titles):
+
+            ax = plt.subplot(*subp); subp[-1] += 1
+            oplot(delta[:, k_point])
+            plt.title(title)
+
+            ax.legend_ = None
+
+    #plt.show()
+
+    diff = compare_deltas(deltas[1:])
+
+    print_diff(diff)
+    try:
+        np.testing.assert_allclose(diff, 0, atol=1e-9)
+    except AssertionError as e:
+        print('The test failed for the parameter set:')
+        p.__dict__.pop("v0")
+        print(p)
+        raise e
+
+    return deltas
+    
+
+#================================================================================ 
+
+if __name__ == '__main__':
+
+    p = ParameterCollection(
+            dim = 1,
+            norbs = 2,
+            t = 2.0,
+            mu = 0.0,
+            beta = 5,
+            U = 1.0,
+            Up = 0.8,
+            J = 0.1,
+            Jp = 0.1,
+            nk = 4,
+            nw = 200,
+            nr_factor = 0.5,
+            fit_const = False,
+            big_factor = 2,
+            )
+
+    for norbs in [1, 2]:
+        p.norbs = norbs
+        deltas = compare_next_delta(p)
+
+    print('The summation and FFT implementation of the eliashberg product'
+                                                ' both yield the same result.')
+
+    p.fit_const = True
+
+    deltas_with_fit = compare_next_delta(p)
+
+    diff = compare_deltas(deltas[2:], deltas_with_fit[2:])
+
+    print('Compare explicit given constant vs. fit:')
+
+    print_diff(diff)
+    np.testing.assert_allclose(diff, 0, atol=1e-9)
+
+    print('Fitting the constant part works.')
+
