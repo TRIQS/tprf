e7213df12a8c5ee121b9fe84c679c42698ee6d6b
Stefan <stefan.kaeser7@gmail.com>
2019-05-13 16:04:45 +0200

    [plot] improve plotting_tools
    - high symmetry points are no longer part of the bsplot implementation
    and the user gives now the k-points himself.
    - axes tuning added
    - dos implementaion added

--- python/triqs_tprf/plotting_tools.py
+++ python/triqs_tprf/plotting_tools.py
@@ -8,56 +8,84 @@ from pytriqs.plot.mpl_interface import plt
 
 from triqs_tprf.lattice_utils import k_space_path
 
-hs_to_k = {
-                'G' : np.array([0.0, 0.0, 0.0]), 
-                'X' : np.array([0.5, 0.0, 0.0]),
-                'Y' : np.array([0.0, 0.5, 0.0]), 
-                'Z' : np.array([0.0, 0.0, 0.5]),
-                'M' : np.array([0.5, 0.5, 0.0]), 
-                'R' : np.array([0.5, 0.5, 0.5]), 
-                        }
+from scipy.stats import gaussian_kde
 
-hs_to_latex = {
-                'G' : r'$\Gamma$',
-                'X' : r'$X$',
-                'Y' : r'$Y$',
-                'Z' : r'$Z$',
-                'M' : r'$M$',
-                'R' : r'$R$',
-                }
+# ========== Bandstructure ========== 
 
 def bsplot(obj, path, *opt_list, **opt_dict):
     """
-    Plot stuff like bs lol
+    Plot Gf objects like bandstructure
     """
-    __bsplot_impl(plt, obj, path, plt.xticks, plt.xticks, *opt_list, **opt_dict)
+    __bsplot_impl(plt, obj, path, *opt_list, **opt_dict)
         
+def __bsplot_impl(top, obj, path, *opt_list, **opt_dict):
 
-def __bsplot_impl(top, obj, path, xticks_fct, xticklabels_fct, *opt_list, **opt_dict):
-
-    hs_points = path.split('-')
-    hs_labels = [hs_to_latex[hs_point] for hs_point in hs_points]
-    hs_k = [hs_to_k[hs_point] for hs_point in hs_points]
+    hs_labels, hs_k = zip(*path)
 
     k_paths = zip(hs_k, hs_k[1:])
     k_vecs, k_plot, K_plot = k_space_path(k_paths, bz=obj.mesh.domain)
     kx, ky, kz = k_vecs.T
-    
-    plt_fct = getattr(top, 'plot')
+
+    # -- If top is plt do now access the currently used Axes object
+    if isinstance(top, types.ModuleType):
+        top = top.gca()
 
     get_gf_on_path = np.vectorize(lambda kx, ky, kz : obj([kx, ky, kz]).real)    
     gf_on_path = get_gf_on_path(kx, ky, kz)
 
-    plt_fct(k_plot, gf_on_path, *opt_list, **opt_dict)
-    
+    top.plot(k_plot, gf_on_path, *opt_list, **opt_dict)
+
+    top.set_xticks(K_plot)
+    top.set_xticklabels(hs_labels)
+
+    # -- Make x-spine cut off at starting and ending high symmetry point
+    top.spines['bottom'].set_bounds(top.get_xticks()[0], top.get_xticks()[-1])
+
+    # -- Make y-spine cut off at highest and lowest value 
+    lower_limit = np.min([line.get_ydata() for line in top.get_lines()])
+    upper_limit = np.max([line.get_ydata() for line in top.get_lines()])
+
+    top.spines['left'].set_bounds(lower_limit, upper_limit)
+    top.set_yticks([lower_limit, upper_limit])
+
+mpl.axes.Axes.bsplot = lambda self, obj, path, *opt_list, **opt_dict : \
+        __bsplot_impl(self, obj, path, *opt_list, **opt_dict)
+
+# ========== DOS ========== 
+
+def dosplot(obj, *opt_list, **opt_dict):
+    """Plot density of states for dispersion relation objects
+    """
+
+    __dosplot_impl(plt, obj, *opt_list, **opt_dict)
+
+def __dosplot_impl(top, obj, *opt_list, **opt_dict):
+
+    lower_limit = np.min(obj.data.real)
+    upper_limit = np.max(obj.data.real)
+
+    dos = gaussian_kde(obj.data[:,0,0].real)
+    xs = np.linspace(lower_limit, upper_limit, 500)
+
+    dos.covariance_factor = lambda : .1
+    dos._compute_covariance()
+
+    # -- If top is plt do now access the currently used Axes object
     if isinstance(top, types.ModuleType):
-        xticks_fct(K_plot, hs_labels)
-    else:
-        xticks_fct(K_plot)
-        xticklabels_fct(hs_labels)
+        top = top.gca()
 
-mpl.axes.Axes.bsplot = lambda self, obj, path, *opt_list, **opt_dict: __bsplot_impl(self, obj, path, self.set_xticks, self.set_xticklabels, *opt_list, **opt_dict)
+    top.plot(dos(xs).real, xs, *opt_list, **opt_dict)
+    top.fill_betweenx(xs, dos(xs).real, [0]*len(xs), alpha=0.25, *opt_list, **opt_dict)
 
+    # -- Make y-spine cut off at highest and lowest value 
+    lower_limit = np.min([line.get_ydata() for line in top.get_lines()])
+    upper_limit = np.max([line.get_ydata() for line in top.get_lines()])
 
+    top.spines['left'].set_bounds(lower_limit, upper_limit)
+    top.set_yticks([lower_limit, upper_limit])
 
+    # -- No x-ticks
+    top.set_xticks([])
 
+mpl.axes.Axes.dosplot = lambda self, obj, *opt_list, **opt_dict : \
+        __dosplot_impl(self, obj, *opt_list, **opt_dict)
