a4b919d6639911472f534005fe991d9271730580
Malte Rösner <m.roesner@science.ru.nl>
2020-02-19 15:34:42 +0100

    MPI/OpenMP hybrid parallelization for Chi00 in real-freq

--- c++/triqs_tprf/lattice/lindhard_chi00.cpp
+++ c++/triqs_tprf/lattice/lindhard_chi00.cpp
@@ -23,6 +23,7 @@
 
 #include "common.hpp"
 #include "lindhard_chi00.hpp"
+#include "../mpi.hpp"
 
 namespace triqs_tprf {
 
@@ -42,57 +43,60 @@ chi_fk_t lindhard_chi00_fk(h_k_cvt h_k, gf_mesh<refreq> mesh, double beta,
 
   chi_fk_t chi_fk{{mesh, kmesh}, {nb, nb, nb, nb}};
   
+  auto fmesh = std::get<0>(chi_fk.mesh());
+  
   for (auto const & [ f, k ] : chi_fk.mesh())
     chi_fk[f, k] = 0.;
     
-  auto fmesh = std::get<0>(chi_fk.mesh());
-  
   std::complex<double> idelta(0.0, delta);
-
-  for (auto const &k : kmesh) {
-
-#pragma omp parallel for 
-    for (int qidx = 0; qidx < kmesh.size(); qidx++) {
     
+  auto arr = mpi_view(kmesh);
+
+  #pragma omp parallel for
+  for (int qidx = 0; qidx < kmesh.size(); qidx++) {
+  
       auto q_iter = kmesh.begin();
       q_iter += qidx;
       auto q = *q_iter;
+  
+      for (int kidx = 0; kidx < arr.size(); kidx++) {
       
-      // -- If this is moved out to the k-loop the threading breaks?!?
-      matrix<std::complex<double>> h_k_mat(h_k[k]);
-      auto eig_k = linalg::eigenelements(h_k_mat);
-      auto ek = eig_k.first;
-      auto Uk = eig_k.second;
-
-      matrix<std::complex<double>> h_kq_mat(h_k(k + q));
-      auto eig_kq = linalg::eigenelements(h_kq_mat);
-      auto ekq = eig_kq.first;
-      auto Ukq = eig_kq.second;
-      
-      // also works ... mu is subtracted just from the diagonal
-      //matrix<std::complex<double>> test_mat(h_k(k + q) - mu);
-
-      for (int i : range(nb)) {
-        for (int j : range(nb)) {
-
-          double de = ekq(j) - ek(i);
-          double dn = fermi((ek(i) - mu) * beta) - fermi((ekq(j) - mu) * beta);
+          auto k = arr(kidx);
           
-          for (auto const &f : fmesh) {
-
-            chi_fk[f, q](a, b, c, d)
-                << chi_fk[f, q](a, b, c, d) + Uk( i, a) * dagger(Uk )(d, i) *
-                                              Ukq(j, c) * dagger(Ukq)(b, j) *
-                                              dn / (f + idelta + de);
-
-          } // f
-
-        }   // j
-      }     // i
-
-    }       // q
-  }         // k
-
+          matrix<std::complex<double>> h_k_mat(h_k(k));
+          auto eig_k = linalg::eigenelements(h_k_mat);
+          auto ek = eig_k.first;
+          auto Uk = eig_k.second;
+          
+          matrix<std::complex<double>> h_kq_mat(h_k(k + q));
+          auto eig_kq = linalg::eigenelements(h_kq_mat);
+          auto ekq = eig_kq.first;
+          auto Ukq = eig_kq.second;
+          
+          for (int i : range(nb)) {
+            for (int j : range(nb)) {
+          
+              double dn = fermi((ek(i) - mu) * beta) - fermi((ekq(j) - mu) * beta);
+              double de = ekq(j) - ek(i);
+              
+              for (auto const &f : fmesh) {
+      
+                chi_fk[f, q](a, b, c, d) 
+                    << chi_fk[f, q](a, b, c, d) + Uk( i, a) * dagger(Uk )(d, i) 
+                                                * Ukq(j, c) * dagger(Ukq)(b, j)
+                                                * dn / (f + idelta + de);
+                  
+              } // f
+              
+          } // i
+        } // j
+          
+      } // k (mpi)
+  
+  } // q (omp)
+  
+  chi_fk = mpi::all_reduce(chi_fk);
+  
   chi_fk /= kmesh.size();
     
   return chi_fk;
