b579381b2ffb36a28c68abd703e302fde16fa258
Stefan <stefan.kaeser7@gmail.com>
2020-08-18 14:40:08 +0200

    [eli] extend preprocess_gamma test

--- test/python/eliashberg/preprocessing_gamma.py
+++ test/python/eliashberg/preprocessing_gamma.py
@@ -5,6 +5,10 @@ from triqs.gf import MeshProduct, MeshImFreq, MeshBrillouinZone, MeshImTime, Mes
 from triqs_tprf.ParameterCollection import ParameterCollection
 from triqs_tprf.utilities import create_eliashberg_ingredients
 from triqs_tprf.eliashberg import split_into_dynamic_wk_and_constant_k, dynamic_and_constant_to_tr
+from triqs_tprf.eliashberg import preprocess_gamma_for_fft 
+
+from triqs_tprf.utilities import assert_parameter_collection_not_equal_model_parameters, write_TarGZ_HDFArchive, read_TarGZ_HDFArchive
+import triqs_tprf.version as version
 
 def test_split_into_dynamic_wk_and_constant_k_mesh_types(gamma):
     gamma_dyn, gamma_const = split_into_dynamic_wk_and_constant_k(gamma)
@@ -34,8 +38,43 @@ def test_dynamic_and_constant_to_tr_mesh_types(gamma):
 
     assert type(gamma_const_r.mesh) == MeshCyclicLattice
 
+def test_preprocess_gamma_for_fft_types(gamma):
+    gamma_dyn_tr, gamma_const_r = preprocess_gamma_for_fft(gamma) 
+
+    assert type(gamma_dyn_tr.mesh) == MeshProduct
+    assert type(gamma_dyn_tr.mesh[0]) == MeshImTime
+    assert type(gamma_dyn_tr.mesh[1]) == MeshCyclicLattice
+
+    assert type(gamma_const_r.mesh) == MeshCyclicLattice
+
+def save_new_preprocess_gamma_for_fft_benchmark(filename, p):
+    eliashberg_ingredients = create_eliashberg_ingredients(p)
+    gamma = eliashberg_ingredients.gamma
+
+    gamma_dyn_tr, gamma_const_r = preprocess_gamma_for_fft(gamma) 
+
+    p.gamma = gamma
+    p.gamma_dyn_tr = gamma_dyn_tr
+    p.gamma_const_r = gamma_const_r 
+
+    write_TarGZ_HDFArchive(filename, p=p)
+
+def test_preprocess_gamma_for_fft_benchmark(gamma, p):
+    p_benchmark = read_TarGZ_HDFArchive(p.benchmark_filename)['p']
+    model_parameters_to_test = ['dim', 'norb', 't', 'mu', 'beta', 'U']
+    assert_parameter_collection_not_equal_model_parameters(p, p_benchmark, model_parameters_to_test)
+
+    np.testing.assert_equal(gamma.data, p_benchmark.gamma.data)
+
+    gamma_dyn_tr, gamma_const_r = preprocess_gamma_for_fft(gamma) 
+    np.testing.assert_equal(gamma_dyn_tr.data, p_benchmark.gamma_dyn_tr.data)
+    np.testing.assert_equal(gamma_const_r.data, p_benchmark.gamma_const_r.data)
+
+
 if __name__ == '__main__':
     p = ParameterCollection(
+            filename = "preprocess_gamma_for_fft_benchmark_new.tar.gz",
+            benchmark_filename = "preprocess_gamma_for_fft_benchmark.tar.gz",
             dim = 1,
             norb = 2,
             t = 2.0,
@@ -47,6 +86,7 @@ if __name__ == '__main__':
             Jp = 0.1,
             nk = 3,
             nw = 500,
+            version_info = version.info,
             )
     eliashberg_ingredients = create_eliashberg_ingredients(p)
     gamma = eliashberg_ingredients.gamma
@@ -56,3 +96,7 @@ if __name__ == '__main__':
     test_split_into_dynamic_wk_and_constant_k_mesh_types(gamma)
     test_split_into_dynamic_wk_and_constant_k_mesh_values(gamma, U_c, U_s)
     test_dynamic_and_constant_to_tr_mesh_types(gamma)
+    test_preprocess_gamma_for_fft_types(gamma)
+
+    #save_new_preprocess_gamma_for_fft_benchmark("preprocess_gamma_for_fft_benchmark.tar.gz", p)
+    test_preprocess_gamma_for_fft_benchmark(gamma, p)
