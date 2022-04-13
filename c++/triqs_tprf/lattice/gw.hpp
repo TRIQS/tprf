be8ada18ff99d05c99d957b51d35150620fa3756
Yann in 't Veld <yann.intveld@ru.nl>
2022-04-13 16:58:02 -0400

    Added documentation the the lindhard_chi00 functions

--- c++/triqs_tprf/lattice/gw.hpp
+++ c++/triqs_tprf/lattice/gw.hpp
@@ -128,9 +128,44 @@ g_wk_t gw_sigma_wk_serial_fft(chi_wk_cvt Wr_wk, g_wk_cvt g_wk);
 
 g_tr_t gw_sigma_tr(chi_tr_cvt Wr_tr, g_tr_cvt g_tr);
 
+/** Real frequency GW self energy :math:`\Sigma(\omega, \mathbf{k})` calculator via the spectral representation
+
+    Computes the spectral function of the dynamic part of the screened interaction
+    
+    .. math::
+        W^{(spec)}_{ab}(\omega, \mathbf{k}) = \frac{-1}{\pi} \text{Im}
+          \left( W_{abab}(\omega, \mathbf{k}) - V_{abab}(\mathbf{k}) \right)
+          
+    and constructs the GW self energy via the spectral representation
+    
+    .. math::
+        \Sigma_{ab}(\omega, \mathbf{k}) = \frac{-1}{N_k} \sum_{\mathbf{q}} \sum_{l}
+          U_{la}(\mathbf{k}+\mathbf{q}) U^\dagger_{bl}(\mathbf{k}+\mathbf{q})
+          V_{abab}(\mathbf{q}) f(\epsilon_{\mathbf{k}+\mathbf{q}, l})
+        + \frac{\delta_{\omega}}{N_k} \sum_{\mathbf{q}} \sum_{\omega^'}
+          U_{la}(\mathbf{k}+\mathbf{q}) U^\dagger_{bl}(\mathbf{k}+\mathbf{q})
+          W^{(spec)}_{ab}(\omega^', \mathbf{q})
+          \frac{n_B(\omega^') + f(\epsilon_{\mathbf{k}+\mathbf{q}, l})}
+          {\omega + i\delta + \omega^' - \epsilon_{\mathbf{k}+\mathbf{q}, l}) + \mu}
+          
+    where the $U(\mathbf{k})$ matrices are the diagonalizing unitary transform of the matrix valued 
+    dispersion relation $\epsilon_{\bar{a}b}(\mathbf{k})$, i.e.
+
+    .. math::
+       \sum_{\bar{a}b} U_{i\bar{a}}(\mathbf{k}) \epsilon_{\bar{a}b}(\mathbf{k}) U^\dagger_{bj} (\mathbf{k})
+       = \delta_{ij} \epsilon_{\mathbf{k}, i}
+       
+    @param mu chemical potential :math:`\mu`
+    @param beta inverse temperature
+    @param e_k discretized lattice dispersion :math:`\epsilon_{\bar{a}b}(\mathbf{k})`
+    @param fmesh real frequency mesh
+    @param Wr_fk fully screened interaction :math:`W_{abcd}(\omega, \mathbf{k})`
+    @param V_k bare interaction :math:`V_{abcd}(\mathbf{k})`
+    @param delta broadening :math:`\delta`
+    @return real frequency GW self-energy :math:`\Sigma_{ab}(\omega, \mathbf{k})`
+*/
+
 g_fk_t gw_sigma_fk_g0w0_spectral(double mu, double beta, e_k_cvt e_k, gf_mesh<refreq> mesh, chi_fk_cvt Wr_fk, chi_k_cvt v_k, double delta);
 
-/** ADD DESCRIPTION
- */
 
 } // namespace triqs_tprf
