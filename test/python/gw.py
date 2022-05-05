0e9750d0da47d93c300b9736fa6ffcfd90977cb2
Yann in 't Veld <yann.intveld@ru.nl>
2022-05-05 17:01:34 -0400

    [gw] Implemented tests for the GW self-energy functions

--- test/python/gw.py
+++ test/python/gw.py
@@ -5,6 +5,16 @@ import numpy as np
 
 # ----------------------------------------------------------------------
 
+from triqs_tprf.lattice import fourier_wk_to_wr
+from triqs_tprf.lattice import fourier_wr_to_wk
+from triqs_tprf.lattice import fourier_wr_to_tr
+from triqs_tprf.lattice import fourier_tr_to_wr
+
+from triqs_tprf.lattice import chi_wr_from_chi_tr
+from triqs_tprf.lattice import chi_tr_from_chi_wr
+from triqs_tprf.lattice import chi_wk_from_chi_wr
+from triqs_tprf.lattice import chi_wr_from_chi_wk 
+
 from triqs_tprf.tight_binding import TBLattice
 
 from triqs_tprf.lattice import lattice_dyson_g0_wk
@@ -15,70 +25,97 @@ from triqs_tprf.gw import dynamical_screened_interaction_W
 from triqs_tprf.gw import gw_sigma
 from triqs_tprf.gw import g0w_sigma
 
-from triqs.gf import Gf, MeshImFreq, Idx
+from triqs.gf import Gf, MeshImFreq, Idx, MeshImTime
+from triqs.gf.mesh_product import MeshProduct
 
 # ----------------------------------------------------------------------
 
-nw = 1000
-nk = 8
-norb = 1
-beta = 10.0
-V = 1.0
-mu = 0.0
+def test_gw_sigma_functions():
+    nw = 1000
+    nk = 8
+    norb = 1
+    beta = 10.0
+    V = 1.0
+    mu = 0.0
+    
+    t = -1.0 * np.eye(norb)
+    
+    t_r = TBLattice(
+        units = [(1, 0, 0)],
+        hopping = {
+            (+1,) : t,
+            (-1,) : t,
+            },
+        orbital_positions = [(0,0,0)]*norb,
+        )
+    
+    kmesh = t_r.get_kmesh(n_k=(nk, 1, 1))
+    e_k = t_r.fourier(kmesh)
+    
+    kmesh = e_k.mesh
+    wmesh = MeshImFreq(beta, 'Fermion', nw)
+    g0_wk = lattice_dyson_g0_wk(mu=mu, e_k=e_k, mesh=wmesh)
+    
+    V_k = Gf(mesh=kmesh, target_shape=[norb]*4)
+    V_k.data[:] = V
+    
+    print('--> pi_bubble')
+    PI_wk = bubble_PI_wk(g0_wk)
+    
+    print('--> dynamical_screened_interaction_W')
+    Wr_dyn_wk = dynamical_screened_interaction_W(PI_wk, V_k)
+    
+    Wr_full_wk = Gf(mesh=Wr_dyn_wk.mesh, target_shape=[norb]*4)
+    for w in Wr_full_wk.mesh.components[0]:
+        Wr_full_wk[w,:] = Wr_dyn_wk[w,:] + V_k
+    
+    print('--> gw_sigma')
+    sigma_wk = gw_sigma(Wr_full_wk, g0_wk)
 
-t = -1.0 * np.eye(norb)
+    print('--> test static and dynamic parts')
+    sigma_dyn_wk = gw_sigma(Wr_dyn_wk, g0_wk)
+    sigma_stat_k = gw_sigma(V_k, g0_wk)
+    sigma_wk_ref = Gf(mesh=sigma_dyn_wk.mesh, target_shape=sigma_dyn_wk.target_shape)
+    for w in wmesh:
+        sigma_wk_ref[w,:].data[:] = sigma_dyn_wk[w,:].data[:] + sigma_stat_k.data[:]
+ 
+    diff = sigma_wk.data[:] - sigma_wk_ref.data[:]
+    print(np.max(np.abs(np.real(diff))))
+    print(np.max(np.abs(np.imag(diff))))
 
-t_r = TBLattice(
-    units = [(1, 0, 0)],
-    hopping = {
-        (+1,) : t,
-        (-1,) : t,
-        },
-    orbital_positions = [(0,0,0)]*norb,
-    )
+    np.testing.assert_array_almost_equal(sigma_wk.data[:], 
+        sigma_wk_ref.data[:])
 
-kmesh = t_r.get_kmesh(n_k=(nk, 1, 1))
-e_k = t_r.fourier(kmesh)
+    print('--> test fourier transforms')
+    ntau = nw*6+1
+    g0_wr = fourier_wk_to_wr(g0_wk)
+    g0_tr = fourier_wr_to_tr(g0_wr, nt=ntau)
+    Wr_wr = chi_wr_from_chi_wk(Wr_dyn_wk)
+    Wr_tr = chi_tr_from_chi_wr(Wr_wr, ntau=ntau)
+  
+    sigma_tr = gw_sigma(Wr_tr, g0_tr)
+    sigma_wr = fourier_tr_to_wr(sigma_tr, nw=nw)
+    sigma_dyn_wk_ref = fourier_wr_to_wk(sigma_wr)
 
-kmesh = e_k.mesh
-wmesh = MeshImFreq(beta, 'Fermion', nw)
-g0_wk = lattice_dyson_g0_wk(mu=mu, e_k=e_k, mesh=wmesh)
+    
+    diff = sigma_dyn_wk.data[:] - sigma_dyn_wk_ref.data[:]
+    print(np.max(np.abs(np.real(diff))))
+    print(np.max(np.abs(np.imag(diff))))
 
-V_k = Gf(mesh=kmesh, target_shape=[norb]*4)
-V_k.data[:] = V
-
-print('--> pi_bubble')
-PI_wk = bubble_PI_wk(g0_wk)
-
-print('--> screened_interaction_W (static bare interaction)')
-Wr_wk = dynamical_screened_interaction_W(PI_wk, V_k)
-
-Wr_full_wk = Gf(mesh=Wr_wk.mesh, target_shape=[norb]*4)
-for w in Wr_full_wk.mesh.components[0]:
-    Wr_full_wk[w,:] = Wr_wk[w,:] + V_k
-
-print('--> gw_self_energy')
-sigma_wk = gw_sigma(Wr_full_wk, g0_wk)
-
-sigma_dyn_wk = gw_sigma(Wr_wk, g0_wk)
-sigma_stat_k = gw_sigma(V_k, g0_wk)
-sigma_wk_ref = Gf(mesh=sigma_dyn_wk.mesh, target_shape=sigma_dyn_wk.target_shape)
-for w in wmesh:
-    sigma_wk_ref[w,:] = sigma_dyn_wk[w,:] + sigma_stat_k
-
-np.testing.assert_array_almost_equal(sigma_wk.data, sigma_wk_ref.data, decimal=5)
-
-
+    np.testing.assert_array_almost_equal(sigma_dyn_wk.data[:], 
+        sigma_dyn_wk_ref.data[:])
+    
+    print('--> g0w_sigma') 
+    sigma_k = gw_sigma(V_k, g0_wk)
+    sigma_k_ref = g0w_sigma(mu=mu, beta=beta, e_k=e_k, v_k=V_k)
+    np.testing.assert_array_almost_equal(sigma_k.data[:], 
+        sigma_k_ref.data[:], decimal=4)
+    
+    print('--> lattice_dyson_g_wk')
+    g_wk = lattice_dyson_g_wk(mu, e_k, sigma_wk)
 
 
 
-sigma_k = gw_sigma(V_k, g0_wk)
-sigma_k_ref = g0w_sigma(mu=mu, beta=beta, e_k=e_k, v_k=V_k)
+if __name__ == "__main__":
+    test_gw_sigma_functions()
 
-np.testing.assert_array_almost_equal(sigma_k.data, sigma_k_ref.data, decimal=4)
-
-
-
-
-print('--> lattice_dyson_g_wk')
-g_wk = lattice_dyson_g_wk(mu, e_k, sigma_wk)
--- test/python/gw_fk.py
+++ test/python/gw_fk.py
@@ -10,7 +10,7 @@ from triqs_tprf.tight_binding import TBLattice
 from triqs_tprf.lattice import lattice_dyson_g0_fk
 from triqs_tprf.lattice import lattice_dyson_g_fk
 from triqs_tprf.lattice import lindhard_chi00_fk
-from triqs_tprf.lattice import gw_sigma_fk_g0w0_spectral, gw_sigma_k_g0w0
+from triqs_tprf.lattice import g0w_sigma
 from triqs_tprf.gw import dynamical_screened_interaction_W
 
 from triqs.gf import Gf, MeshReFreq, inverse
@@ -18,55 +18,61 @@ from triqs.gf.mesh_product import MeshProduct
 
 # ----------------------------------------------------------------------
 
-nw = 2 #101
-wmin = -0.00001
-wmax = +0.00001
-nk = 5
-norb = 1
-beta = 10.0
-V = 1.0
-mu = 0.0
-delta = 0.001
+def test_gw_self_energy_real_freq():
+    nw = 2 #101
+    wmin = -0.00001
+    wmax = +0.00001
+    nk = 5
+    norb = 1
+    beta = 10.0
+    V = 1.0
+    mu = 0.0
+    delta = 0.001
+    
+    t = -1.0 * np.eye(norb)
+    
+    t_r = TBLattice(
+        units = [(1, 0, 0)],
+        hopping = {
+            (+1,) : t,
+            (-1,) : t,
+            },
+        orbital_positions = [(0,0,0)]*norb,
+        )
+    
+    kmesh = t_r.get_kmesh(n_k=(nk, 1, 1))
+    e_k = t_r.fourier(kmesh)
+    
+    
+    kmesh = e_k.mesh
+    fmesh = MeshReFreq(wmin, wmax, nw)
+    g_fk = lattice_dyson_g0_fk(mu=mu, e_k=e_k, mesh=fmesh, delta=delta)
+    
+    V_k = Gf(mesh=kmesh, target_shape=[norb]*4)
+    V_k.data[:] = V
+    
+    V_fk = Gf(mesh=g_fk.mesh, target_shape=[norb]*4)
+    V_fk.data[:] = V
+    
+    print('--> pi_bubble')
+    PI_fk = lindhard_chi00_fk(e_k=e_k, mesh=fmesh, beta=beta, mu=mu, delta=delta)
+    
+    print('--> screened_interaction_W')
+    Wr_fk = dynamical_screened_interaction_W(PI_fk, V_k) 
+    
+    print('--> gw_self_energy')
+    sigma_fk = g0w_sigma(mu=mu, beta=beta, e_k=e_k, mesh=fmesh,
+        W_fk=Wr_fk, v_k=V_k, delta=delta)
+    sigma_k = g0w_sigma(mu=mu, beta=beta, e_k=e_k, v_k=V_k)
+    
+    # Check if sigma_fk and sigma_k are the same at w=0
+    f0_ind = len(fmesh)//2
+    assert np.allclose( list(fmesh.values())[f0_ind], 0.0, atol=wmax )
+    np.testing.assert_array_almost_equal(sigma_fk.data[f0_ind,:], sigma_k.data[:])
+    
+    print('--> lattice_dyson_g_wk')
+    g_fk = lattice_dyson_g_fk(mu, e_k, sigma_fk, delta)
 
-t = -1.0 * np.eye(norb)
 
-t_r = TBLattice(
-    units = [(1, 0, 0)],
-    hopping = {
-        (+1,) : t,
-        (-1,) : t,
-        },
-    orbital_positions = [(0,0,0)]*norb,
-    )
-
-kmesh = t_r.get_kmesh(n_k=(nk, 1, 1))
-e_k = t_r.fourier(kmesh)
-
-
-kmesh = e_k.mesh
-fmesh = MeshReFreq(wmin, wmax, nw)
-g_fk = lattice_dyson_g0_fk(mu=mu, e_k=e_k, mesh=fmesh, delta=delta)
-
-V_k = Gf(mesh=kmesh, target_shape=[norb]*4)
-V_k.data[:] = V
-
-V_fk = Gf(mesh=g_fk.mesh, target_shape=[norb]*4)
-V_fk.data[:] = V
-
-print('--> pi_bubble')
-PI_fk = lindhard_chi00_fk(e_k=e_k, mesh=fmesh, beta=beta, mu=mu, delta=delta)
-
-print('--> screened_interaction_W')
-Wr_fk = dynamical_screened_interaction_W(PI_fk, V_k) 
-
-print('--> gw_self_energy')
-sigma_fk = gw_sigma_fk_g0w0_spectral(mu=mu, beta=beta, e_k=e_k, mesh=fmesh, Wr_fk=Wr_fk, v_k=V_k, delta=delta)
-sigma_k = gw_sigma_k_g0w0(mu=mu, beta=beta, e_k=e_k, v_k=V_k)
-
-# Check if sigma_fk and sigma_k are the same at w=0
-f0_ind = len(fmesh)//2
-assert np.allclose( list(fmesh.values())[f0_ind], 0.0, atol=wmax )
-np.testing.assert_array_almost_equal(sigma_fk.data[f0_ind,:], sigma_k.data[:])
-
-print('--> lattice_dyson_g_wk')
-g_fk = lattice_dyson_g_fk(mu, e_k, sigma_fk, delta)
+if __name__ == "__main__":
+    test_gw_self_energy_real_freq()
