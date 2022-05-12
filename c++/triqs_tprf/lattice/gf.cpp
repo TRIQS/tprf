267ffb93e27ce14e587c0c6232011381f50a4bf3
Yann in 't Veld <yann.intveld@ru.nl>
2022-05-12 12:46:58 -0400

    [gf] Slight change of Gf initialization to 0

--- c++/triqs_tprf/lattice/gf.cpp
+++ c++/triqs_tprf/lattice/gf.cpp
@@ -43,7 +43,7 @@ g_wk_t lattice_dyson_g0_wk(double mu, e_k_cvt e_k, gf_mesh<imfreq> mesh) {
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_wk_t g0_wk({mesh, e_k.mesh()}, e_k.target_shape());
-  g0_wk *= 0.;
+  g0_wk() = 0.0;
     
   auto arr = mpi_view(g0_wk.mesh());
 
@@ -63,7 +63,7 @@ g_wk_t lattice_dyson_g0_wk(double mu, e_k_cvt e_k, gf_mesh<imfreq> mesh) {
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_wk_t g0_wk({mesh, e_k.mesh()}, e_k.target_shape());
-  g0_wk *= 0.;
+  g0_wk() = 0.0;
   
   for (auto const &[w, k] : mpi_view(g0_wk.mesh()))
       g0_wk[w, k] = inverse((w + mu)*I - e_k(k));
@@ -82,7 +82,7 @@ g_fk_t lattice_dyson_g0_fk(double mu, e_k_cvt e_k, gf_mesh<refreq> mesh,
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_fk_t g0_fk({mesh, e_k.mesh()}, e_k.target_shape());
-  g0_fk *= 0.;
+  g0_fk() = 0.0;
   std::complex<double> idelta(0.0, delta);
     
   auto arr = mpi_view(g0_fk.mesh());
@@ -103,7 +103,7 @@ g_wk_t lattice_dyson_g_wk(double mu, e_k_cvt e_k, g_wk_cvt sigma_wk) {
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   auto g_wk = make_gf(sigma_wk);
-  g_wk *= 0.;
+  g_wk() = 0.0;
 
   for (auto const &[w, k] : mpi_view(g_wk.mesh()) ) 
     g_wk[w, k] = inverse((w + mu)*I - e_k[k] - sigma_wk[w, k]);
@@ -119,7 +119,7 @@ g_wk_t lattice_dyson_g_wk(double mu, e_k_cvt e_k, g_w_cvt sigma_w) {
   auto mesh = sigma_w.mesh();
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_wk_t g_wk({sigma_w.mesh(), e_k.mesh()}, e_k.target_shape());
-  g_wk *= 0.;
+  g_wk() = 0.0;
 
   auto arr = mpi_view(g_wk.mesh());
 
@@ -140,7 +140,7 @@ g_wk_t lattice_dyson_g_wk(double mu, e_k_cvt e_k, g_w_cvt sigma_w) {
   auto mesh = sigma_w.mesh();
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_wk_t g_wk({mesh, e_k.mesh()}, e_k.target_shape());
-  g_wk *= 0.;
+  g_wk() = 0.0;
 
   for (auto const &[w, k] : mpi_view(g_wk.mesh()) ) 
     g_wk[w, k] = inverse((w + mu)*I - e_k(k) - sigma_w[w]);
@@ -159,7 +159,7 @@ g_fk_t lattice_dyson_g_fk(double mu, e_k_cvt e_k, g_fk_cvt sigma_fk,
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   auto g_fk = make_gf(sigma_fk);
-  g_fk *= 0.;
+  g_fk() = 0.0;
   std::complex<double> idelta(0.0, delta);
     
   auto arr = mpi_view(g_fk.mesh());
@@ -184,7 +184,7 @@ g_w_t lattice_dyson_g_w(double mu, e_k_cvt e_k, g_w_cvt sigma_w) {
 
   auto I = nda::eye<ek_vt::scalar_t>(e_k.target_shape()[0]);
   g_w_t g_w(wmesh, e_k.target_shape());
-  g_w *= 0.;
+  g_w() = 0.0;
 
   auto wkmesh = gk_iw_t::mesh_t{{wmesh, kmesh}};
   
