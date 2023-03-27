84c8ad5fa7176fc03a46a0afcc94646e26b30f0f
Hugo U. R. Strand <hugo.strand@gmail.com>
2023-03-27 11:21:29 +0200

    [rpa_rt] WIP

--- test/python/rpa_rt.py
+++ test/python/rpa_rt.py
@@ -23,17 +23,134 @@ from triqs_tprf.lattice import fourier_Tr_to_Tk
 from triqs_tprf.lattice import fourier_Tk_to_Tr
 from triqs_tprf.lattice import chi0_Tr_from_g_Tr_PH
 
+from triqs_tprf.rpa_rt import chi0_tr_from_g_tr_les_gtr
+from triqs_tprf.rpa_rt import chi0_tr_les_gtr_from_g_tr_les_gtr
+
 from triqs_tprf.lattice import dynamical_screened_interaction_W
 
+# Gregory coefficients of order 32
 
-def test_fft(verbose=False):
+greg = np.array([
+-7.8024383965700760956707426885259337723255157470703125E-01,
++1.7203004564829256217706188181182369589805603027343750E+00,
+-1.1293914328292293802746826258953660726547241210937500E+01,
++6.9040528844601595892527257092297077178955078125000000E+01,
+-3.5287838015253959156325436197221279144287109375000000E+02,
++1.5001985068704459536093054339289665222167968750000000E+03,
+-5.3601248928468357917154207825660705566406250000000000E+03,
++1.6285104073565818907809443771839141845703125000000000E+04,
+-4.2508001142523455200716853141784667968750000000000000E+04,
++9.6140272797904486651532351970672607421875000000000000E+04,
+-1.8969528355236799689009785652160644531250000000000000E+05,
++3.2828785580717795528471469879150390625000000000000000E+05,
+-5.0036565636323939543217420578002929687500000000000000E+05,
++6.7370192565594415646046400070190429687500000000000000E+05,
+-8.0294330893410148564726114273071289062500000000000000E+05,
++8.4807946549424529075622558593750000000000000000000000E+05,
+-7.9404676912516623269766569137573242187500000000000000E+05,
++6.5866466407562396489083766937255859375000000000000000E+05,
+-4.8334433653750858502462506294250488281250000000000000E+05,
++3.1302010741073323879390954971313476562500000000000000E+05,
+-1.7827630392221597139723598957061767578125000000000000E+05,
++8.8870604585172768565826117992401123046875000000000000E+04,
+-3.8533790420769262709654867649078369140625000000000000E+04,
++1.4414059713544615078717470169067382812500000000000000E+04,
+-4.6019186226658493978902697563171386718750000000000000E+03,
++1.2363601752227505130576901137828826904296875000000000E+03,
+-2.7421022400211342073816922493278980255126953125000000E+02,
++4.8877487889135068144241813570261001586914062500000000E+01,
+-6.7303812118926353846859456098172813653945922851562500E+00,
++6.7198303653292179582479093369329348206520080566406250E-01,
+-4.3290258602310616176112034736433997750282287597656250E-02,
++1.3509659123127626401128509314730763435363769531250000E-03,
+    ])
 
-    for zero_padding in [0, 1, 2, 3]:
+def fourier_trasform_of_spectral_function(t, f, g_f):
+
+    Nt = len(t)
+    Nf = len(f)
+    zero_padding = (2*Nf) // Nt - 1
+    df = f[1] - f[0]
+
+    print(f'Nt = {len(t)}, Nf = {len(f)}, zero_padding = {zero_padding}')
+    
+    A_f = - g_f.imag / np.pi
+
+    norm_trapz = np.trapz(A_f, x=f)
+    print(f'norm_trapz = {norm_trapz}')
+
+    norm_riemann = np.sum(A_f) * df
+    print(f'norm_riemann = {norm_riemann}')
+
+    #N = 2 * (2 - zero_padding) * Nf
+    N = 4 * Nf
+    print(f'N = {N}')
+    window = np.blackman(Nf-1)
+    window[:] = 1.
+    #window = 1.
+
+    norm = np.trapz(A_f[1:] * window, x=f[1:])
+    print(f'norm = {norm}')
+
+    #A_f /= norm
+
+    
+    #A_t = -1.j * np.fft.ifft(np.fft.ifftshift(A_f[:-1] * window, axes=0), n=N, axis=0)
+    #A_t = -1.j * np.fft.fft(A_f[1:] * window, n=N, axis=0)
+    A_t = -1.j * np.fft.fft(A_f[1:] * window, n=N, axis=0)
+
+    N2 = N // (2 * (1 + zero_padding))
+    A_t = A_t[:N2]
+
+    #A_t /= (2. * np.pi)**2
+    A_t *= df
+
+    # -- Boundary corrections using Gregory weights
+
+    #f = df * np.arange(
+    
+    k = len(greg)
+    mat_tf = np.exp(1j*f[None, 1:k+1]*t[:, None]) * df * greg[None, :]
+    #A_t += np.tensordot(mat_tf, A_f[1:k+1], axes=(1, 0))
+
+    mat_tf = np.exp(1j*f[None, -k-1:-1]*t[:, None]) * df * greg[None, :]
+    #A_t += np.tensordot(mat_tf, A_f[-k-1:-1], axes=(1, 0))
+    
+    t_shift = np.exp(1.j * t * f[-1])
+    A_t *= t_shift
+    
+    if False:
+        import matplotlib.pyplot as plt
+        subp = [2, 1, 1]
+
+        plt.subplot(*subp); subp[-1] += 1
+        #plt.plot(t, t_shift.imag)
+        plt.plot(t, A_t.real)
+        plt.plot(t, A_t.imag)
+        #plt.xlim([0, 10])
+        plt.subplot(*subp); subp[-1] += 1
+        #plt.plot(f, A_f * window)
+        plt.plot(A_f)
+        plt.plot(np.fft.fftshift(A_f[:-1]))
+        plt.show(); exit()
+    
+    return A_t
+    
+def fourier_from_fX_to_tX(G_fX):
+    pass
+
+def test_fft(verbose=True):
+
+    #for zero_padding in [0, 1, 2, 3]:
+    for zero_padding in [0]:
 
         print(f'zero_padding = {zero_padding}')
 
-        dt = 0.04
-        Nt = 2**10
+        #dt = 0.0002
+        #Nt = 2**16
+
+        dt = 0.04 / 2**10
+        Nt = 2**10 * 2**10
 
         t = dt * np.arange(Nt)
         tmesh = MeshReTime(t[0], t[-1], len(t))
@@ -42,16 +159,17 @@ def test_fft(verbose=False):
         t = np.array(list(tmesh.values()))
         f = np.array(list(fmesh.values()))
 
-        e = 1 - 0.5j
+        e = 0 - 0.5j
+        #e = 1. - 0.5j
         g_t = -1j * np.exp(-1j * e * t)
 
         g_f = fourier_transformation_with_heaviside_matrix(f, t, g_t)
         g_f_ref = 1/(f - e)
 
-        diff = np.max(np.abs(g_f - g_f_ref))
+        diff_f = np.max(np.abs(g_f - g_f_ref))
         print(f'Nt = {len(t)}, Nf = {len(f)}, zero_padding = {zero_padding}')
-        print(f'diff = {diff:2.2E}')
-        np.testing.assert_array_almost_equal(g_f, g_f_ref, decimal=7)
+        print(f'diff_f = {diff_f:2.2E}')
+        #np.testing.assert_array_almost_equal(g_f, g_f_ref, decimal=7)
 
         G_t = Gf(mesh=tmesh, target_shape=[1])
         G_t.data[:, 0] = -1j * np.exp(-1j * e * t)
@@ -62,23 +180,40 @@ def test_fft(verbose=False):
 
         diff = np.max(np.abs(np.squeeze(G_f.data) - g_f_ref))
         print(f'diff = {diff:2.2E}')
-        np.testing.assert_array_almost_equal(
-            np.squeeze(G_f.data), g_f_ref, decimal=7)
+        #np.testing.assert_array_almost_equal(
+        #    np.squeeze(G_f.data), g_f_ref, decimal=7)
+
+        # -- Frequency to time
+        g_t_ref = fourier_trasform_of_spectral_function(t, f, g_f)
+        #G_t_ref = fourier_from_fX_to_tX(G_f)
+
+        diff_t = np.max(np.abs(g_t - g_t_ref))
+        print(f'diff_t = {diff_t:2.2E}')
+        
+        
 
     if verbose:
         
         import matplotlib.pyplot as plt
 
-        plt.figure(figsize=(8, 10))
+        plt.figure(figsize=(8, 9))
 
-        subp = [4, 2, 1]
+        subp = [5, 2, 1]
 
         plt.subplot(*subp); subp[-1] += 1
         plt.plot(t, g_t.real)
+        plt.plot(t, g_t_ref.real, '--')
 
         plt.subplot(*subp); subp[-1] += 1
         plt.plot(t, g_t.imag)
+        plt.plot(t, g_t_ref.imag, '--')
 
+        plt.subplot(*subp); subp[-1] += 1
+        plt.plot(t, g_t.real - g_t_ref.real)
+
+        plt.subplot(*subp); subp[-1] += 1
+        plt.plot(t, g_t.imag - g_t_ref.imag)
+        
         plt.subplot(*subp); subp[-1] += 1
         plt.plot(t, np.abs(g_t.real))
         plt.semilogy([], [])
@@ -107,7 +242,7 @@ def test_fft(verbose=False):
         plt.show()
 
 
-def test_chi0(verbose=False):
+def test_chi0(verbose=True):
 
     beta = 100.0
     mu = 0.0
@@ -118,8 +253,8 @@ def test_chi0(verbose=False):
     kappa = 20.0
     t = 1.0
 
-    Nt = 256
-    dt = 0.1
+    Nt = 256 * 2
+    dt = 0.1 / 2
     zero_padding = 0
     delta = 0.4
 
@@ -156,22 +291,32 @@ def test_chi0(verbose=False):
     g_tr_gtr = fourier_Tk_to_Tr(g_tk_gtr)
     print(f'done {time.time() - tmr} s')
 
-    del g_tk_les
-    del g_tk_gtr
+    #del g_tk_les
+    #del g_tk_gtr
     
     tmr = time.time()
     print('--> chi0_tr from g')
     chi0_tr = chi0_Tr_from_g_Tr_PH(g_tr_les, g_tr_gtr)
     print(f'done {time.time() - tmr} s')
 
-    del g_tr_les
-    del g_tr_gtr
+    chi0_tr_ref = chi0_tr_from_g_tr_les_gtr(g_tr_les, g_tr_gtr)
+    np.testing.assert_array_almost_equal(chi0_tr.data, chi0_tr_ref.data)
+
+    chi0_tr_les, chi0_tr_gtr = chi0_tr_les_gtr_from_g_tr_les_gtr(g_tr_les, g_tr_gtr)
+    chi0_tr_ref2 = chi0_tr_les - chi0_tr_gtr
+    np.testing.assert_array_almost_equal(chi0_tr.data, chi0_tr_ref2.data)
+    
+    #del g_tr_les
+    #del g_tr_gtr
     
     tmr = time.time()
     print('--> fourier_Tr_to_Tk (chi0)')
     chi0_tk = fourier_Tr_to_Tk(chi0_tr)
     print(f'done {time.time() - tmr} s')
 
+    chi0_tk_les = fourier_Tr_to_Tk(chi0_tr_les)
+    chi0_tk_gtr = fourier_Tr_to_Tk(chi0_tr_gtr)
+    
     tmr = time.time()
     print('--> fourier_from_tX_to_fX (chi0)')
     chi0_fk = fourier_from_tX_to_fX(
@@ -235,6 +380,12 @@ def test_chi0(verbose=False):
     pi_tk = chi0_tk.copy()
     pi_tk.data[:] = -1.0 * pi_tk.data[:]
 
+    pi_tk_les = chi0_tk_les.copy()
+    pi_tk_les.data[:] = -1.0 * pi_tk_les.data[:]
+
+    pi_tk_gtr = chi0_tk_gtr.copy()
+    pi_tk_gtr.data[:] = -1.0 * pi_tk_gtr.data[:]
+    
     pi_fk = chi0_fk.copy()
     pi_fk.data[:] = -1.0 * pi_fk.data[:]
 
@@ -247,6 +398,21 @@ def test_chi0(verbose=False):
     Wc_tk = W_tk_RPA(pi_tk, V_k)
     print(f'done {time.time() - tmr} s')
 
+    Wc_tk_les = W_tk_RPA(pi_tk_les, V_k)
+    Wc_tk_gtr = W_tk_RPA(pi_tk_gtr, V_k)
+
+    Wc_tk_ref = Wc_tk_gtr - Wc_tk_les
+    np.testing.assert_array_almost_equal(Wc_tk.data, Wc_tk_ref.data)
+    exit()
+    
+    tmr = time.time()
+    print('--> fourier_from_tk_to_tr Wc')
+    Wc_tr = fourier_Tk_to_Tr(Wc_tk)
+    print(f'done {time.time() - tmr} s')
+
+    Wc_tr_les = fourier_Tk_to_Tr(Wc_tk_les)
+    Wc_tr_gtr = fourier_Tk_to_Tr(Wc_tk_gtr)
+    
     tmr = time.time()
     print('--> fourier_from_tk_to_fk chi0')
     Wc_fk = fourier_from_tX_to_fX(Wc_tk, zero_padding=zero_padding, windowing=False)
@@ -260,10 +426,37 @@ def test_chi0(verbose=False):
     W_diff = np.max(np.abs(W_f - W_f_ref))
     print(f'W_diff = {W_diff:2.2E}')
     np.testing.assert_array_almost_equal(W_f, W_f_ref, decimal=2)
+
+    from triqs_tprf.lattice import g0w_sigma
+    print(f'--> sigma_fk'); tmr = time.time()
+    sigma_fk_ref = g0w_sigma(mu, beta, e_k, W_fk, V_k, delta)
+    print(f'done {time.time() - tmr} s')
+
+    sigma_f_ref = interpolate_g_fk(sigma_fk_ref, k0)
+
+    # -- Real time Sigma GW
+    sigma_tr_les = g_tr_les.copy()
+    sigma_tr_les.data[:] *= 1j * Wc_tr_les.data[..., 0, 0]
+
+    sigma_tr_gtr = g_tr_gtr.copy()
+    sigma_tr_gtr.data[:] *= 1j * Wc_tr_gtr.data[..., 0, 0]
+    
+    sigma_tr = sigma_tr_gtr - sigma_tr_les
+
+    sigma_tk = fourier_Tr_to_Tk(sigma_tr)
+    sigma_fk = fourier_from_tX_to_fX(
+            sigma_tk, zero_padding=zero_padding, windowing=False)
+
+    #sigma_fk.data[:] += V_k.data[None, :, :, :, 0, 0] * g_tk_les.data[0, ...][None, ...]
+    #sigma_fk.data[:] *= 0.
+    
+    sigma_f = interpolate_g_fk(sigma_fk, k0)
     
     if verbose:
         import matplotlib.pyplot as plt
-        subp = [3, 1, 1]
+        plt.figure(figsize=(6, 8))
+        
+        subp = [4, 1, 1]
 
         plt.subplot(*subp); subp[-1] += 1
         plt.plot(f, chi0_f.real, label='Re')
@@ -285,11 +478,20 @@ def test_chi0(verbose=False):
         plt.plot(f, np.abs(W_f - W_f_ref))
         plt.ylabel(r'$\Delta W(k_{small}, \omega)$')
         plt.legend(loc='best')
-        
+
+        plt.subplot(*subp); subp[-1] += 1
+        plt.plot(f, sigma_f.real, label='Re')
+        plt.plot(f, sigma_f.imag, label='Im')
+        plt.plot(f, sigma_f_ref.real, '--', label='Re', alpha=0.75)
+        plt.plot(f, sigma_f_ref.imag, '--', label='Im', alpha=0.75)
+        plt.ylabel(r'$\Sigma(k_{small}, \omega)$')
+        plt.legend(loc='best')
+
+        plt.tight_layout()
         plt.show()
 
     
 if __name__ == "__main__":
         
-    test_fft()
+    #test_fft()
     test_chi0()
