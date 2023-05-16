50175b14bddeed9e281ca9be8c002d766fe7edf2
Yann in 't Veld <yann.intveld@ru.nl>
2023-05-16 11:19:17 +0200

    [g0w] Implemented single kpoint calculation for g0w_dyn_sigma

--- c++/triqs_tprf/lattice/gw.hpp
+++ c++/triqs_tprf/lattice/gw.hpp
@@ -156,6 +156,14 @@ namespace triqs_tprf {
 
   g_tr_t gw_dynamic_sigma(chi_tr_cvt W_tr, g_tr_cvt g_tr);
 
+  /** some documentation */
+
+  g_f_t g0w_dyn_sigma(double mu, double beta, e_k_cvt e_k, chi_fk_cvt W_fk, chi_k_cvt v_k, double delta, mesh::brzone::point_t kpoint);
+
+  /** some documentation */
+  
+  g_fk_t g0w_dyn_sigma(double mu, double beta, e_k_cvt e_k, chi_fk_cvt W_fk, chi_k_cvt v_k, double delta, gf_mesh<brzone> kmesh);
+
   /** Real frequency GW self energy :math:`\Sigma(\omega, \mathbf{k})` calculator via the spectral representation
 
     Computes the spectral function of the dynamic part of the screened interaction
