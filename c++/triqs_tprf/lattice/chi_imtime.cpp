4f2788616a0038f4b5640f0a372e383397cfd060
Hugo U. R. Strand <hugo.strand@gmail.com>
2025-03-07 15:45:12 +0100

    [latt] chi0_w0r_from_grt_PH DLR sign fix

--- c++/triqs_tprf/lattice/chi_imtime.cpp
+++ c++/triqs_tprf/lattice/chi_imtime.cpp
@@ -147,7 +147,7 @@ chi_t_t::target_t::value_t integrate_dlr_tau(chi_Dt_cvt chi_t) {
     auto w = chi_x.mesh().dlr_freq()[l];
     auto k0 = cppdlr::k_it(0, w);
     auto k1 = cppdlr::k_it(1, w);
-    I += chi_x.mesh().beta() * (k1 - k0) / w * chi_x[l];
+    I += chi_x.mesh().beta() * (k0 - k1) / w * chi_x[l];
   }
 
   return I;
