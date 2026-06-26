b0b67291841b25fc951adc3f464a3d37491e5f94
Erik van Loon (laptop) <erik.van_loon@teorfys.lu.se>
2026-06-26 17:18:28 +0200

    Different choice of parameters in Hubbard-Zeeman example

--- doc/user_guide/dbse_hubbard_zeeman/calc_sc_dmft.py
+++ doc/user_guide/dbse_hubbard_zeeman/calc_sc_dmft.py
@@ -33,9 +33,9 @@ from triqs_tprf.ParameterCollection import ParameterCollections
 p = ParameterCollection(
     t = 1.,
     t_prime = 0.,
-    B = 0.5,
+    B = 1,
     crystal_field = 0.,
-    U = 0.5,
+    U = 4,
     N_target=1.0,
     n_l = 30,
     n_k = 8,
